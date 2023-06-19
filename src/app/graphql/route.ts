9a92d573077c35fa4bde71216e0263609172a2ba
Jedddy <jedbalita25@gmail.com>
2023-06-19 18:10:49 +0800

    init: initialize graphql backend

---
+++ src/app/graphql/route.ts
@@ -0,0 +1,31 @@
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { ApolloServer } from '@apollo/server';
+import { gql } from 'graphql-tag';
+import { NextRequest } from 'next/server';
+
+const resolvers = {
+  Query: {
+    hello: async () => 'world',
+  }
+};
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+});
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server);
+
+export async function GET(request: NextRequest) {
+  return handler(request);
+}
+
+export async function POST(request: NextRequest) {
+  return handler(request);
+}
