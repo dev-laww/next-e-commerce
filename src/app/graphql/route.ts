1849cee525ba94d5476e83a9bd32a81911637abd
Jedddy <jedbalita25@gmail.com>
2023-06-19 18:46:19 +0800

    feat: create initial category create/request

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -2,16 +2,45 @@ import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { ApolloServer } from '@apollo/server';
 import { gql } from 'graphql-tag';
 import { NextRequest } from 'next/server';
+import prisma from '../lib/prisma';
 
 const resolvers = {
   Query: {
-    hello: async () => 'world',
+    categories: async () => prisma.category.findMany(),
+  },
+  Mutation: {
+    createCategory: async (_: undefined, {name, description}: {name: string, description: string}) => {
+      return prisma.category.create({
+        data: {
+          name: name,
+          description: description
+        }
+      })
+    }
   }
 };
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    categories: [Category!]!
+  }
+
+  type Mutation {
+    createCategory(name: String, description: String): Category!
+  }
+
+  type Category {
+    id: Int
+    name: String!
+    description: String!
+    products: [Product]
+  }
+
+  type Product {
+    id: Int
+    name: String
+    description: String
+    category: Category
   }
 `;
 
