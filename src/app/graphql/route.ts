3f22849756e0c5c15ce74d6fc8f0f6da3ea01f17
Jedddy <jedbalita25@gmail.com>
2023-06-20 13:02:53 +0800

    chore: separated typedefs and resolvers

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -1,48 +1,9 @@
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { ApolloServer } from '@apollo/server';
-import { gql } from 'graphql-tag';
 import { NextRequest } from 'next/server';
-import prisma from '../lib/prisma';
+import resolvers from '@src/lib/resolvers';
+import typeDefs from '@src/lib/typedefs';
 
-const resolvers = {
-  Query: {
-    categories: async () => prisma.category.findMany(),
-  },
-  Mutation: {
-    createCategory: async (_: undefined, {name, description}: {name: string, description: string}) => {
-      return prisma.category.create({
-        data: {
-          name: name,
-          description: description
-        }
-      })
-    }
-  }
-};
-
-const typeDefs = gql`
-  type Query {
-    categories: [Category!]!
-  }
-
-  type Mutation {
-    createCategory(name: String, description: String): Category!
-  }
-
-  type Category {
-    id: Int
-    name: String!
-    description: String!
-    products: [Product]
-  }
-
-  type Product {
-    id: Int
-    name: String
-    description: String
-    category: Category
-  }
-`;
 
 const server = new ApolloServer({
   resolvers,
