215dc8bbf04329725e6d6be8ece1ad31bea01d6f
tora-o <97511605+tora-o@users.noreply.github.com>
2023-07-23 21:33:50 +0800

    chore: finished adding seeders

---
+++ prisma/seed.ts
@@ -0,0 +1,88 @@
+import { PrismaClient } from '@prisma/client';
+
+import seeders from './seeders';
+
+const prisma = new PrismaClient()
+
+interface SeedMap {
+    [key: string]: any;
+}
+
+
+async function seedEntity(entityName: string, findCondition: any, data: any[]): Promise<void> {
+
+    let seedMap: SeedMap = {
+        'addresses': prisma.address,
+        'carts': prisma.cartItem,
+        'categories': prisma.category,
+        'coupons': prisma.coupon,
+        'logs': prisma.log,
+        'orderItems': prisma.orderItem,
+        'orders': prisma.order,
+        'paymentMethods': prisma.paymentMethod,
+        'payments': prisma.payment,
+        'productCategories': prisma.productCategory,
+        'productVariants': prisma.productVariant,
+        'products': prisma.product,
+        'shippingMethods': prisma.shippingMethod,
+        'users': prisma.user,
+        'reviews': prisma.review,
+        'wishlists': prisma.wishlistItem,
+    }
+
+    const func = seedMap[entityName];
+
+    if (!func) {
+        console.log(`Invalid entity name: ${entityName}`);
+        return;
+    }
+
+
+    const entity = await func.findFirst({where: findCondition});
+
+    if (entity) return;
+
+    await func.createMany({data: data});
+}
+
+
+async function main() {
+    console.log('Start seeding ...')
+
+    const nonRelationEntities: any = [
+        seeders.users,
+        seeders.products,
+        seeders.categories,
+        seeders.coupons,
+        seeders.logs,
+        seeders.shippingMethods,
+    ];
+
+    for (const [entityName, data] of Object.entries(seeders)) {
+        if (!nonRelationEntities.includes(data)) {
+            continue;
+        }
+
+        await seedEntity(entityName, {id: 1000}, data);
+    }
+
+    for (const [entityName, data] of Object.entries(seeders)) {
+        if (nonRelationEntities.includes(data)) {
+            continue;
+        }
+
+        await seedEntity(entityName, {id: 1000}, data);
+    }
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+        console.info('Seeding done.')
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
+
---
+++ prisma/seeders/index.ts
@@ -0,0 +1,37 @@
+import addresses from './addresses';
+import carts from './cart';
+import categories from './categories';
+import coupons from './coupons';
+import logs from './logs';
+import orderItems from './order-items';
+import orders from './orders';
+import paymentMethods from './payment-methods';
+import payments from './payments';
+import productCategories from './product-categories';
+import productVariants from './product-variants';
+import products from './products';
+import reviews from './reviews';
+import shippingMethods from './shipping-methods';
+import users from './users';
+import wishlists from './wishlist';
+
+
+export default {
+    users,
+    products,
+    categories,
+    coupons,
+    shippingMethods,
+    logs,
+    addresses,
+    productVariants,
+    productCategories,
+    orders,
+    orderItems,
+    paymentMethods,
+    payments,
+    reviews,
+    carts,
+    wishlists
+}
+
