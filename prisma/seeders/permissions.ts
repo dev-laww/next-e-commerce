bbbfab3b2e782d4f9cb346942144b502aebc2260
Jedddy <jedbalita25@gmail.com>
2023-07-24 22:56:57 +0800

    chore: add role and permission seeders

---
+++ prisma/seeders/permissions.ts
@@ -0,0 +1,188 @@
+const permissions = [
+    {
+        id: 1,
+        name: "CREATE_USER"
+    },
+    {
+        id: 2,
+        name: "UPDATE_USER"
+    },
+    {
+        id: 3,
+        name: "DELETE_USER"
+    },
+    {
+        id: 4,
+        name: "VIEW_USER"
+    },
+    {
+        id: 5,
+        name: "CREATE_PRODUCT"
+    },
+    {
+        id: 6,
+        name: "UPDATE_PRODUCT"
+    },
+    {
+        id: 7,
+        name: "DELETE_PRODUCT"
+    },
+    {
+        id: 8,
+        name: "VIEW_PRODUCT"
+    },
+    {
+        id: 9,
+        name: "CREATE_ROLE"
+    },
+    {
+        id: 10,
+        name: "UPDATE_ROLE"
+    },
+    {
+        id: 11,
+        name: "DELETE_ROLE"
+    },
+    {
+        id: 12,
+        name: "VIEW_ROLE"
+    },
+    {
+        id: 13,
+        name: "ASSIGN_ROLE"
+    },
+    {
+        id: 14,
+        name: "ADD_ADDRESS"
+    },
+    {
+        id: 15,
+        name: "UPDATE_ADDRESS"
+    },
+    {
+        id: 16,
+        name: "DELETE_ADDRESS"
+    },
+    {
+        id: 17,
+        name: "CREATE_CATEGORY"
+    },
+    {
+        id: 18,
+        name: "UPDATE_CATEGORY"
+    },
+    {
+        id: 19,
+        name: "DELETE_CATEGORY"
+    },
+    {
+        id: 20,
+        name: "VIEW_CATEGORY"
+    },
+    {
+        id: 21,
+        name: "CREATE_COUPON"
+    },
+    {
+        id: 22,
+        name: "UPDATE_COUPON"
+    },
+    {
+        id: 23,
+        name: "DELETE_COUPON"
+    },
+    {
+        id: 24,
+        name: "VIEW_COUPON"
+    },
+    {
+        id: 25,
+        name: "VIEW_LOGS"
+    },
+    {
+        id: 26,
+        name: "DELETE_LOGS"
+    },
+    {
+        id: 27,
+        name: "CREATE_REVIEWS"
+    },
+    {
+        id: 28,
+        name: "UPDATE_REVIEWS"
+    },
+    {
+        id: 29,
+        name: "DELETE_REVIEWS"
+    },
+    {
+        id: 30,
+        name: "VIEW_REVIEWS"
+    },
+    {
+        id: 31,
+        name: "ADD_PAYMENT_METHOD"
+    },
+    {
+        id: 32,
+        name: "UPDATE_PAYMENT_METHOD"
+    },
+    {
+        id: 33,
+        name: "DELETE_PAYMENT_METHOD"
+    },
+    {
+        id: 34,
+        name: "VIEW_PAYMENT_METHOD"
+    },
+    {
+        id: 35,
+        name: "ADD_WISHLIST"
+    },
+    {
+        id: 36,
+        name: "UPDATE_WISHLIST"
+    },
+    {
+        id: 37,
+        name: "DELETE_WISHLIST"
+    },
+    {
+        id: 38,
+        name: "VIEW_WISHLIST"
+    },
+    {
+        id: 39,
+        name: "ADD_SHIPPING_METHOD"
+    },
+    {
+        id: 40,
+        name: "UPDATE_SHIPPING_METHOD"
+    },
+    {
+        id: 41,
+        name: "DELETE_SHIPPING_METHOD"
+    },
+    {
+        id: 42,
+        name: "VIEW_SHIPPING_METHOD"
+    },
+    {
+        id: 43,
+        name: "CREATE_ORDER"
+    },
+    {
+        id: 44,
+        name: "UPDATE_ORDER"
+    },
+    {
+        id: 45,
+        name: "DELETE_ORDER"
+    },
+    {
+        id: 46,
+        name: "VIEW_ORDER"
+    }
+]
+
+export default permissions;
---
+++ prisma/seeders/roles.ts
@@ -0,0 +1,16 @@
+const roles = [
+    {
+        id: 1,
+        name: "ADMIN"
+    },
+    {
+        id: 2,
+        name: "SUBADMIN"
+    },
+    {
+        id: 2,
+        name: "CUSTOMER"
+    }
+]
+
+export default roles;
