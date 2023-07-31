63df8c1cac0524b8750f0545021dd0bafdb6af20
tora-o <97511605+tora-o@users.noreply.github.com>
2023-07-31 20:52:53 +0800

    chore: created new migration files

---
+++ pages/api/hello.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    return res.status(200).json({
+        status: 'success',
+        message: 'Hello World!'
+    });
+}
