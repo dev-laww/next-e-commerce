396a0cd4ed62c6d4a3a830d375dae865374e19d0
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-03 16:24:51 +0800

    feat: finished auth endpoints

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import AuthController from "@controller/auth_controller";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const controller = new AuthController();
+
+    const {statusCode, response} = await controller.signup(req);
+
+    return res.status(statusCode).json(response);
+}
