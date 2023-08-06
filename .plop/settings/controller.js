31fad86d89ff757873cfaf260d5ac6b0d99811d4
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-06 22:25:52 +0800

    enhancement: made the file kebab case

--- .plop/settings/controller.js
+++ .plop/settings/controller.js
@@ -12,25 +12,25 @@ module.exports = {
         // controller
         {
             type: "add",
-            path: "../src/controller/{{camelCase name}}.controller.ts",
+            path: "../src/controller/{{kebabCase name}}.controller.ts",
             templateFile: "templates/controller/controller.ts.hbs"
         },
         // tests
         {
             type: "add",
-            path: "../__test__/unit/controller/{{camelCase name}}.controller.test.ts",
+            path: "../__test__/unit/controller/{{kebabCase name}}.controller.test.ts",
             templateFile: "templates/controller/test.controller.ts.hbs"
         },
         // validation
         {
             type: "add",
-            path: "../src/lib/validator/{{camelCase name}}.validator.ts",
+            path: "../src/lib/validator/{{kebabCase name}}.validator.ts",
             templateFile: "templates/controller/validator.ts.hbs"
         },
         // validation tests
         {
             type: "add",
-            path: "../__test__/unit/validator/{{camelCase name}}.validator.test.ts",
+            path: "../__test__/unit/validator/{{kebabCase name}}.validator.test.ts",
             templateFile: "templates/controller/test.validator.ts.hbs"
         }
     ]
