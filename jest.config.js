80759f950050263e4bdee4742db7c3cda246b3fc
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-09 21:24:22 +0800

    fix: jest config fix

--- jest.config.js
+++ jest.config.js
@@ -24,12 +24,14 @@ const config = {
         "@src/(.*)$": "<rootDir>/src/$1",
         "^@components/(.*)$": "<rootDir>/src/components/$1",
         "^@app/(.*)$": "<rootDir>/src/app/$1",
-        "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+        "^@utils/(.*)$": "<rootDir>/src/lib/utils/$1",
         "^@lib/(.*)$": "<rootDir>/src/lib/$1",
         "^@repository/(.*)$": "<rootDir>/src/repository/$1",
         "^@controller/(.*)$": "<rootDir>/src/controller/$1",
         "^@mocks/(.*)$": "<rootDir>/__mocks__/$1",
-    }
+    },
+    verbose: true,
+    silent: true,
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
