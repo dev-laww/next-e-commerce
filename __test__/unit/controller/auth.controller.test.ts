ede91cfc4fbeda8780f9927805fdd72ba60634dd
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-09 21:25:50 +0800

    test: updated tests for controller

--- __test__/unit/controller/auth.controller.test.ts
+++ __test__/unit/controller/auth.controller.test.ts
@@ -1,12 +1,17 @@
-import AuthController from "@controller/auth.controller";
 import { NextApiRequest } from "next";
-import { User } from "@prisma/client";
+
+import AuthController from "@controller/auth.controller";
+import { TokenOTP, User } from "@prisma/client";
+import * as Constants from "@lib/constants";
+import Email from "@utils/email";
 
 jest.mock("@repository/user.repo", () => require("@mocks/repository/user.repo.mock"));
+jest.mock("@utils/email", () => require("@mocks/lib/utils/email.mock"));
 
 describe("Auth Controller", () => {
     let controller: AuthController;
     const data = {
+        id: 1,
         username: "username",
         password: "$2a$10$FyDL9cB8vaD9U/XF4Q81A.m2W26BHjT/fXkjw1xQtgKQ4z6dH7/Zu",
         email: "email",
@@ -44,7 +49,7 @@ describe("Auth Controller", () => {
             const {statusCode, response} = await controller.login(req);
 
             // expect
-            expect(statusCode).toBe(200);
+            expect(statusCode).toBe(Constants.STATUS_CODE.SUCCESS);
             expect(response.data).toBeDefined();
             expect(response.data.accessToken).toBeDefined();
         })
@@ -62,7 +67,7 @@ describe("Auth Controller", () => {
             const {statusCode, response} = await controller.login(req);
 
             // expect
-            expect(statusCode).toBe(401);
+            expect(statusCode).toBe(Constants.STATUS_CODE.UNAUTHORIZED);
             expect(response.data).toBeUndefined();
         })
 
@@ -71,15 +76,107 @@ describe("Auth Controller", () => {
             const {statusCode, response} = await controller.login(req);
 
             // expect
-            expect(statusCode).toBe(422);
+            expect(statusCode).toBe(Constants.STATUS_CODE.UNPROCESSABLE_ENTITY);
             expect(response.data).toBeUndefined();
         });
+
+        it("returns 400 if invalid request method", async () => {
+            req.method = "GET";
+
+            // call login
+            const {statusCode, response} = await controller.login(req);
+
+            // expect
+            expect(statusCode).toBe(Constants.STATUS_CODE.BAD_REQUEST);
+            expect(response.data).toBeUndefined();
+        })
     })
 
     describe("Test signup", () => {
-        it.todo("returns 200 if register success");
-        it.todo("returns 422 if wrong body");
-        it.todo("returns 409 if user already exist");
+        let req: NextApiRequest;
+
+        beforeEach(() => {
+            req = {
+                method: "POST",
+                body: {
+                    username: "username",
+                    password: "password",
+                    confirmPassword: "password",
+                    email: "email@mail.com",
+                    firstName: "name",
+                    lastName: "name",
+                    imageUrl: "https://image.com/image.jpg"
+                }
+            } as NextApiRequest;
+
+            jest.clearAllMocks();
+        });
+
+        it("returns 201 if register success", async () => {
+            (controller.userRepo.getUserByEmail as jest.Mock).mockResolvedValue(null);
+            (controller.userRepo.getUserById as jest.Mock).mockResolvedValue(null);
+            (controller.userRepo.generateTokenOTP as jest.Mock).mockResolvedValue({
+                id: 1,
+                user_id: 1,
+                token: "x",
+                type: Constants.TOKEN_TYPE.EMAIL_CONFIRMATION_OTP,
+            } as TokenOTP);
+            (controller.userRepo.createUser as jest.Mock).mockResolvedValue(data);
+
+            const {statusCode, response} = await controller.signup(req);
+
+            expect(statusCode).toBe(Constants.STATUS_CODE.CREATED);
+            expect(response.data).toBeDefined();
+        });
+
+        it("returns 422 if wrong body", async () => {
+            req.body = {};
+
+            const {statusCode, response} = await controller.signup(req);
+
+            expect(statusCode).toBe(Constants.STATUS_CODE.UNPROCESSABLE_ENTITY);
+            expect(response.data).toBeUndefined();
+        });
+
+        it("returns 400 if user already exist", async () => {
+            (controller.userRepo.getUserByEmail as jest.Mock).mockResolvedValue(data);
+            (controller.userRepo.getUserById as jest.Mock).mockResolvedValue(data);
+
+            const {statusCode, response} = await controller.signup(req);
+
+            expect(statusCode).toBe(Constants.STATUS_CODE.BAD_REQUEST);
+            expect(response.data).toBeUndefined();
+        });
+
+        it("returns 500 if generation token fail", async () => {
+            (controller.userRepo.getUserByEmail as jest.Mock).mockResolvedValue(null);
+            (controller.userRepo.getUserById as jest.Mock).mockResolvedValue(null);
+            (controller.userRepo.generateTokenOTP as jest.Mock).mockResolvedValue(null);
+            (controller.userRepo.createUser as jest.Mock).mockResolvedValue(data);
+
+            const {statusCode, response} = await controller.signup(req);
+
+            expect(statusCode).toBe(Constants.STATUS_CODE.INTERNAL_SERVER_ERROR);
+            expect(response.data).toBeUndefined();
+        });
+
+        it("returns 500 if token sending fail", async () => {
+            (controller.userRepo.getUserByEmail as jest.Mock).mockResolvedValue(null);
+            (controller.userRepo.getUserById as jest.Mock).mockResolvedValue(null);
+            (controller.userRepo.generateTokenOTP as jest.Mock).mockResolvedValue({
+                id: 1,
+                user_id: 1,
+                token: "x",
+                type: Constants.TOKEN_TYPE.EMAIL_CONFIRMATION_OTP,
+            } as TokenOTP);
+            (controller.userRepo.createUser as jest.Mock).mockResolvedValue(data);
+            (Email.sendToken as jest.Mock).mockRejectedValue(Error("error"));
+
+            const {statusCode, response} = await controller.signup(req);
+
+            expect(statusCode).toBe(Constants.STATUS_CODE.INTERNAL_SERVER_ERROR);
+            expect(response.data).toBeUndefined();
+        })
     })
 
     describe("Test reset password", () => {
