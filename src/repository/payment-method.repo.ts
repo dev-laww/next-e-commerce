443156c06b66eca12bd212c11ba83c3ea0e3fde5
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-14 15:02:08 +0800

    feat: finished creating payment-method repo

--- src/repository/payment-method.repo.ts
+++ src/repository/payment-method.repo.ts
@@ -1,12 +1,48 @@
-import { Prisma } from "@prisma/client";
+import { PaymentMethod, Prisma } from "@prisma/client";
 
 import prisma from "@lib/prisma";
 
 export default class PaymentMethodRepository {
     prismaClient = prisma;
 
-    // TODO: Implement repository methods
-    public async test() {
-        return "Hello World";
+    // TODO: Add pagination
+    public async getAll(filter: Prisma.PaymentMethodWhereInput | undefined = undefined): Promise<PaymentMethod[]> {
+        return this.prismaClient.paymentMethod.findMany({
+            where: filter,
+            include: {
+                _count: true
+            }
+        });
+    }
+
+    public async getById(id: number): Promise<PaymentMethod | null> {
+        return this.prismaClient.paymentMethod.findUnique({
+            where: {id: id}
+        });
+    }
+
+    public async getUserPaymentMethods(userId: number): Promise<PaymentMethod[] | null> {
+        return this.prismaClient.paymentMethod.findMany({
+            where: {user_id: userId}
+        });
+    }
+
+    public async create(paymentMethod: Prisma.PaymentMethodCreateInput): Promise<PaymentMethod> {
+        return this.prismaClient.paymentMethod.create({
+            data: paymentMethod
+        });
+    }
+
+    public async update(id: number, paymentMethod: Prisma.PaymentMethodUpdateInput): Promise<PaymentMethod> {
+        return this.prismaClient.paymentMethod.update({
+            where: {id: id},
+            data: paymentMethod
+        });
+    }
+
+    public async delete(id: number): Promise<PaymentMethod> {
+        return this.prismaClient.paymentMethod.delete({
+            where: {id: id}
+        });
     }
 }
