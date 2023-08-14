b89ecb9b2d8c966439b75b0e161a7af170851a0d
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-14 15:02:35 +0800

    refactor: minor refactors

--- __test__/unit/repository/payment-method.repo.test.ts
+++ __test__/unit/repository/payment-method.repo.test.ts
@@ -3,7 +3,6 @@ import PaymentMethodRepository from "@repository/payment-method.repo";
 describe("PaymentMethodRepository", () => {
     it.todo("Test getAll");
     it.todo("Test getById");
-    it.todo("Test getByUserId")
     it.todo("Test getUserPaymentMethods")
     it.todo("Test create");
     it.todo("Test update");
