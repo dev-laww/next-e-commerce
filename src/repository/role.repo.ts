f779cc18dede3e0cc117f71628f4061be26913e1
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-17 14:43:48 +0800

    feat: finished role repository

--- src/repository/role.repo.ts
+++ src/repository/role.repo.ts
@@ -1,12 +1,107 @@
-import { Prisma } from "@prisma/client";
+import { Permission, Prisma, Role } from "@prisma/client";
 
 import prisma from "@lib/prisma";
 
 export default class RoleRepository {
     prismaClient = prisma;
+    public async getAll(filter: Prisma.RoleWhereInput | undefined = undefined): Promise<Role[]> {
+        return this.prismaClient.role.findMany({
+            where: filter,
+        });
+    }
 
-    // TODO: Implement repository methods
-    public async test() {
-        return "Hello World";
+    public async getById(id: number): Promise<Role | null> {
+        return this.prismaClient.role.findUnique({
+            where: {
+                id: id
+            }
+        });
+    }
+
+    public async create(data: Role): Promise<Role> {
+        return this.prismaClient.role.create({
+            data: data
+        });
+    }
+
+    public async update(id: number, data: Role): Promise<Role> {
+        return this.prismaClient.role.update({
+            where: {
+                id: id
+            },
+            data: data
+        });
+    }
+
+    public async delete(id: number): Promise<Role> {
+        return this.prismaClient.role.delete({
+            where: {
+                id: id
+            }
+        });
+    }
+
+    public async getRolePermissions(id: number): Promise<Permission[]> {
+        const rolePermissions = await this.prismaClient.rolePermission.findMany({
+            where: {
+                role_id: id
+            },
+            select: {
+                permission: true
+            }
+        });
+
+        return rolePermissions.map(rolePermission => {
+            const permission = rolePermission.permission;
+
+            return {
+                id: permission.id,
+                name: permission.name,
+                description: permission.description
+            } as Permission;
+        });
+    }
+
+    public async addRolePermissions(id: number, permissions: number[]): Promise<Permission[]> {
+        await this.prismaClient.role.update({
+            where: {
+                id: id
+            },
+            data: {
+                permissions: {
+                    connect: permissions.map(permission => ({id: permission}))
+                }
+            }
+        });
+
+        return this.getRolePermissions(id);
+    }
+
+    public async updateRolePermissions(id: number, permissions: number[]): Promise<Permission[]> {
+         await this.prismaClient.role.update({
+            where: {
+                id: id
+            },
+            data: {
+                permissions: {
+                    set: permissions.map(permission => ({id: permission}))
+                }
+            }
+        });
+
+        return this.getRolePermissions(id);
+    }
+
+    public async deleteRolePermissions(id: number, permissions: number[]): Promise<Role> {
+        return this.prismaClient.role.update({
+            where: {
+                id: id
+            },
+            data: {
+                permissions: {
+                    disconnect: permissions.map(permission => ({id: permission}))
+                }
+            }
+        });
     }
 }
