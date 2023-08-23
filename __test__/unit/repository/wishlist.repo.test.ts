a9914c5b7683682ad4723667795c0c5a824648b3
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-23 21:32:47 +0800

    test: created tests for wishlist repo

--- __test__/unit/repository/wishlist.repo.test.ts
+++ __test__/unit/repository/wishlist.repo.test.ts
@@ -1,4 +1,6 @@
 import WishlistRepository from "@repository/wishlist.repo";
+import prisma from "@lib/prisma";
+import { Prisma } from "@prisma/client";
 
 jest.mock("@lib/prisma", require("@mocks/lib/prisma.mock"));
 
@@ -9,13 +11,74 @@ describe("WishlistRepository", () => {
         jest.clearAllMocks();
     });
 
-    it.todo("Test getAll");
-    it.todo("Test getById");
-    it.todo("Test getUserWishlist");
-    it.todo("Test getByProductId");
-    it.todo("Test create");
-    it.todo("Test update");
-    it.todo("Test delete");
-    it.todo("Test deleteUserWishlist");
-    it.todo("Test deleteByProductId");
+    it("Test getAll", async () => {
+        (prisma.wishlistItem.findMany as jest.Mock).mockResolvedValueOnce([]);
+
+        let result = await repo.getAll();
+
+        expect(result).toMatchObject([]);
+    });
+
+    it("Test getById", async () => {
+        (prisma.wishlistItem.findUnique as jest.Mock).mockResolvedValueOnce(null);
+
+        let result = await repo.getById(1);
+
+        expect(result).toEqual(null);
+    });
+
+    it("Test getUserWishlist", async () => {
+        (prisma.wishlistItem.findMany as jest.Mock).mockResolvedValueOnce([]);
+
+        let result = await repo.getUserWishlist(1);
+
+        expect(result).toMatchObject([]);
+    });
+    it("Test getByProductId", async () => {
+        (prisma.wishlistItem.findMany as jest.Mock).mockResolvedValueOnce([]);
+
+        let result = await repo.getByProductId(1);
+
+        expect(result).toMatchObject([]);
+    });
+
+    it("Test create", async () => {
+        (prisma.wishlistItem.create as jest.Mock).mockResolvedValueOnce({});
+
+        let result = await repo.create({} as Prisma.WishlistItemCreateInput);
+
+        expect(result).toEqual({});
+    });
+
+    it("Test update", async () => {
+        (prisma.wishlistItem.update as jest.Mock).mockResolvedValueOnce({});
+
+        let result = await repo.update(1, {} as Prisma.WishlistItemUpdateInput);
+
+        expect(result).toEqual({});
+    });
+
+    it("Test delete", async () => {
+        (prisma.wishlistItem.delete as jest.Mock).mockResolvedValueOnce({});
+
+        let result = await repo.delete(1);
+
+        expect(result).toEqual({});
+    });
+
+    it("Test deleteUserWishlist", async () => {
+        (prisma.wishlistItem.deleteMany as jest.Mock).mockResolvedValueOnce({});
+
+        let result = await repo.deleteUserWishlist(1);
+
+        expect(result).toEqual({});
+    });
+
+    it("Test deleteByProductId", async () => {
+        (prisma.wishlistItem.deleteMany as jest.Mock).mockResolvedValueOnce({});
+
+        let result = await repo.deleteByProductId(1);
+
+        expect(result).toEqual({});
+    });
 });
