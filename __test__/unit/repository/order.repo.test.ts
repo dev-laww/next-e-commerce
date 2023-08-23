b7d172c2648c0582bfd5826ca68f8d0051b9034b
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-23 21:20:33 +0800

    test: created tests for order repo

--- __test__/unit/repository/order.repo.test.ts
+++ __test__/unit/repository/order.repo.test.ts
@@ -1,4 +1,7 @@
 import OrderRepository from "@repository/order.repo";
+import prisma from "@lib/prisma";
+import { ORDER_STATUS } from "@lib/constants";
+import { Order } from "@prisma/client";
 
 jest.mock("@lib/prisma", require("@mocks/lib/prisma.mock"));
 
@@ -9,14 +12,100 @@ describe("OrderRepository", () => {
         jest.clearAllMocks();
     });
 
-    it.todo("Test getAll");
-    it.todo("Test getById");
-    it.todo("Test getUserOrders");
-    it.todo("Test getByStatusId");
-    it.todo("Test getByAddressId");
-    it.todo("Test getByShippingId");
-    it.todo("Test getItems");
-    it.todo("Test create");
-    it.todo("Test update");
-    it.todo("Test delete");
+    it("Test getAll", async () => {
+        (prisma.order.findMany as jest.Mock).mockResolvedValueOnce([]);
+
+        const result = await repo.getAll();
+        expect(result).toEqual([]);
+    });
+
+    it("Test getById", async () => {
+        (prisma.order.findUnique as jest.Mock).mockResolvedValueOnce({
+            id: 1,
+            status: ORDER_STATUS.COMPLETED,
+        });
+
+        const result = await repo.getById(1);
+        expect(result).toEqual({
+            id: 1,
+            status: ORDER_STATUS.COMPLETED,
+        });
+    });
+
+    it("Test getUserOrders", async () => {
+        (prisma.order.findMany as jest.Mock).mockResolvedValueOnce([]);
+
+        const result = await repo.getUserOrders(1);
+        expect(result).toEqual([]);
+    });
+
+    it("Test getByStatus", async () => {
+        (prisma.order.findMany as jest.Mock).mockResolvedValueOnce([]);
+
+        const result = await repo.getByStatus(ORDER_STATUS.COMPLETED);
+        expect(result).toEqual([]);
+    });
+
+    it("Test getByAddressId", async () => {
+        (prisma.order.findMany as jest.Mock).mockResolvedValueOnce([]);
+
+        const result = await repo.getByAddressId(1);
+        expect(result).toEqual([]);
+    });
+
+    it("Test getByShippingId", async () => {
+        (prisma.order.findMany as jest.Mock).mockResolvedValueOnce([]);
+
+        const result = await repo.getByShippingId(1);
+        expect(result).toEqual([]);
+    });
+
+    it("Test getItems", async () => {
+        (prisma.order.findUnique as jest.Mock).mockResolvedValueOnce(null);
+
+        let result = await repo.getItems(1);
+        expect(result).toEqual([]);
+
+        (prisma.order.findUnique as jest.Mock).mockResolvedValueOnce({
+            order_items: [{
+                id: 1,
+                order_id: 1,
+                product_id: 1,
+                quantity: 1,
+                price: 1,
+            }]
+        });
+
+        result = await repo.getItems(1);
+        expect(result).toEqual([{
+            id: 1,
+            order_id: 1,
+            product_id: 1,
+            quantity: 1,
+            price: 1,
+        }]);
+    });
+
+    it("Test create", async () => {
+        (prisma.order.create as jest.Mock).mockResolvedValueOnce(null);
+
+        const result = await repo.create({} as Order);
+        expect(result).toEqual(null);
+    });
+
+    it("Test update", async () => {
+        (prisma.order.update as jest.Mock).mockResolvedValueOnce(null);
+
+        const result = await repo.update(1, {
+            status: ORDER_STATUS.PROCESSING,
+        });
+        expect(result).toEqual(null);
+    });
+
+    it("Test delete", async () => {
+        (prisma.order.delete as jest.Mock).mockResolvedValueOnce(null);
+
+        const result = await repo.delete(1);
+        expect(result).toEqual(null);
+    });
 });
