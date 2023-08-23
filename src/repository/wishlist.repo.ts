7323daccb37ecdd2b9d6732e2b7212c0e42e86c3
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-23 21:29:36 +0800

    feat: finished creating wishlist repository

--- src/repository/wishlist.repo.ts
+++ src/repository/wishlist.repo.ts
@@ -1,12 +1,63 @@
-import { Prisma } from "@prisma/client";
+import { Prisma, WishlistItem } from "@prisma/client";
 
 import prisma from "@lib/prisma";
 
 export default class WishlistRepository {
     prismaClient = prisma;
 
-    // TODO: Implement repository methods
-    public async test() {
-        return "Hello World";
+    // TODO: Add pagination
+    public async getAll(filter?: Prisma.WishlistItemWhereInput): Promise<WishlistItem[]> {
+        return this.prismaClient.wishlistItem.findMany({
+            where: filter,
+        });
+    }
+
+    public async getById(id: number): Promise<WishlistItem | null> {
+        return this.prismaClient.wishlistItem.findUnique({
+            where: {id: id},
+        });
+    }
+
+    public async getUserWishlist(userId: number): Promise<WishlistItem[]> {
+        return this.prismaClient.wishlistItem.findMany({
+            where: {user_id: userId},
+        });
+    }
+
+    public async getByProductId(productId: number): Promise<WishlistItem[]> {
+        return this.prismaClient.wishlistItem.findMany({
+            where: {product_id: productId},
+        });
+    }
+
+    public async create(data: Prisma.WishlistItemCreateInput | WishlistItem): Promise<WishlistItem> {
+        return this.prismaClient.wishlistItem.create({
+            data: data
+        });
+    }
+
+    public async update(id: number, data: Prisma.WishlistItemUpdateInput): Promise<WishlistItem> {
+        return this.prismaClient.wishlistItem.update({
+            where: {id: id},
+            data: data
+        });
+    }
+
+    public async delete(id: number): Promise<WishlistItem> {
+        return this.prismaClient.wishlistItem.delete({
+            where: {id: id}
+        });
+    }
+
+    public async deleteUserWishlist(userId: number): Promise<Prisma.BatchPayload> {
+        return this.prismaClient.wishlistItem.deleteMany({
+            where: {user_id: userId}
+        });
+    }
+
+    public async deleteByProductId(productId: number): Promise<Prisma.BatchPayload> {
+        return this.prismaClient.wishlistItem.deleteMany({
+            where: {product_id: productId}
+        });
     }
 }
