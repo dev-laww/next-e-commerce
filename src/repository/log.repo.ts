0f531049944731b21a3717a603edf52015c15aa4
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-23 20:00:15 +0800

    feat: created log repo methods

--- src/repository/log.repo.ts
+++ src/repository/log.repo.ts
@@ -1,12 +1,26 @@
-import { Prisma } from "@prisma/client";
+import { Log, Prisma } from "@prisma/client";
 
 import prisma from "@lib/prisma";
 
 export default class LogRepository {
     prismaClient = prisma;
 
-    // TODO: Implement repository methods
-    public async test() {
-        return "Hello World";
+    // TODO: Add pagination
+    public async getAll(filter?: Prisma.LogWhereInput) {
+        return this.prismaClient.log.findMany({
+            where: filter
+        });
+    }
+
+    public async getById(id: number): Promise<Log | null> {
+        return this.prismaClient.log.findUnique({
+            where: { id }
+        });
+    }
+
+    public async create(data: Prisma.LogCreateInput): Promise<Log> {
+        return this.prismaClient.log.create({
+            data
+        });
     }
 }
