1990710574974b3bb7c7636e33bfaf5eca6caec2
tora-o <97511605+tora-o@users.noreply.github.com>
2023-08-28 15:23:00 +0800

    feat: implemented logging

--- src/app/api/auth/confirm-reset-password/route.ts
+++ src/app/api/auth/confirm-reset-password/route.ts
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import AuthController from "@controller/auth.controller";
+import { getLogger } from "@utils/logging";
 
 async function handler(req: NextRequest) {
+    const logger = getLogger('api:auth:confirm-reset-password');
     const controller = new AuthController();
 
     const {statusCode, response} = await controller.confirmResetPassword(req);
+    await logger.info(statusCode == 200 ? response.message : `Confirm reset password failed: ${response.message}`);
 
     return NextResponse.json(response, {status: statusCode})
 }
--- src/app/api/auth/refresh/route.ts
+++ src/app/api/auth/refresh/route.ts
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import AuthController from "@controller/auth.controller";
+import { getLogger } from "@utils/logging";
 
 async function handler(req: NextRequest) {
+    const logger = getLogger('api:auth:confirm-email');
     const controller = new AuthController();
 
     const {statusCode, response} = await controller.refreshToken(req);
+    await logger.info(statusCode == 200 ? response.data.message : `Refresh token failed: ${response.message}`);
 
     return NextResponse.json(response, {status: statusCode});
 }
