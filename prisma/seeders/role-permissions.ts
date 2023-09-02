5a27150dc8437a750f9111875659247282143eeb
tora-o <97511605+tora-o@users.noreply.github.com>
2023-09-02 17:11:46 +0800

    chore: created role-permission seeder

--- prisma/seeders/role-permissions.ts
+++ prisma/seeders/role-permissions.ts
@@ -1,3 +1,539 @@
-const rolePermissions: any[] = []; // TODO: Add role permissions
+const rolePermissions = [
+    {
+        id: 1000,
+        role_id: 1000,
+        permission_id: 1000
+    },
+    {
+        id: 1001,
+        role_id: 1001,
+        permission_id: 1000
+    },
+    {
+        id: 1002,
+        role_id: 1000,
+        permission_id: 1001
+    },
+    {
+        id: 1003,
+        role_id: 1000,
+        permission_id: 1002
+    },
+    {
+        id: 1004,
+        role_id: 1001,
+        permission_id: 1002
+    },
+    {
+        id: 1005,
+        role_id: 1000,
+        permission_id: 1003
+    },
+    {
+        id: 1006,
+        role_id: 1000,
+        permission_id: 1004
+    },
+    {
+        id: 1007,
+        role_id: 1000,
+        permission_id: 1005
+    },
+    {
+        id: 1008,
+        role_id: 1001,
+        permission_id: 1005
+    },
+    {
+        id: 1009,
+        role_id: 1000,
+        permission_id: 1006
+    },
+    {
+        id: 1010,
+        role_id: 1001,
+        permission_id: 1006
+    },
+    {
+        id: 1011,
+        role_id: 1000,
+        permission_id: 1007
+    },
+    {
+        id: 1012,
+        role_id: 1001,
+        permission_id: 1007
+    },
+    {
+        id: 1013,
+        role_id: 1000,
+        permission_id: 1008
+    },
+    {
+        id: 1014,
+        role_id: 1001,
+        permission_id: 1008
+    },
+    {
+        id: 1015,
+        role_id: 1000,
+        permission_id: 1009
+    },
+    {
+        id: 1016,
+        role_id: 1001,
+        permission_id: 1009
+    },
+    {
+        id: 1017,
+        role_id: 1000,
+        permission_id: 1010
+    },
+    {
+        id: 1018,
+        role_id: 1001,
+        permission_id: 1010
+    },
+    {
+        id: 1019,
+        role_id: 1000,
+        permission_id: 1011
+    },
+    {
+        id: 1020,
+        role_id: 1001,
+        permission_id: 1011
+    },
+    {
+        id: 1021,
+        role_id: 1000,
+        permission_id: 1012
+    },
+    {
+        id: 1022,
+        role_id: 1001,
+        permission_id: 1012
+    },
+    {
+        id: 1023,
+        role_id: 1000,
+        permission_id: 1013
+    },
+    {
+        id: 1024,
+        role_id: 1001,
+        permission_id: 1013
+    },
+    {
+        id: 1025,
+        role_id: 1000,
+        permission_id: 1014
+    },
+    {
+        id: 1026,
+        role_id: 1001,
+        permission_id: 1014
+    },
+    {
+        id: 1027,
+        role_id: 1000,
+        permission_id: 1015
+    },
+    {
+        id: 1028,
+        role_id: 1001,
+        permission_id: 1015
+    },
+    {
+        id: 1029,
+        role_id: 1000,
+        permission_id: 1016
+    },
+    {
+        id: 1030,
+        role_id: 1001,
+        permission_id: 1016
+    },
+    {
+        id: 1031,
+        role_id: 1000,
+        permission_id: 1017
+    },
+    {
+        id: 1032,
+        role_id: 1001,
+        permission_id: 1017
+    },
+    {
+        id: 1033,
+        role_id: 1000,
+        permission_id: 1018
+    },
+    {
+        id: 1034,
+        role_id: 1001,
+        permission_id: 1018
+    },
+    {
+        id: 1035,
+        role_id: 1000,
+        permission_id: 1019
+    },
+    {
+        id: 1036,
+        role_id: 1000,
+        permission_id: 1020
+    },
+    {
+        id: 1037,
+        role_id: 1001,
+        permission_id: 1020
+    },
+    {
+        id: 1038,
+        role_id: 1000,
+        permission_id: 1021
+    },
+    {
+        id: 1039,
+        role_id: 1000,
+        permission_id: 1022
+    },
+    {
+        id: 1040,
+        role_id: 1000,
+        permission_id: 1023
+    },
+    {
+        id: 1041,
+        role_id: 1001,
+        permission_id: 1023
+    },
+    {
+        id: 1042,
+        role_id: 1000,
+        permission_id: 1024
+    },
+    {
+        id: 1043,
+        role_id: 1000,
+        permission_id: 1025
+    },
+    {
+        id: 1044,
+        role_id: 1000,
+        permission_id: 1026
+    },
+    {
+        id: 1045,
+        role_id: 1001,
+        permission_id: 1026
+    },
+    {
+        id: 1046,
+        role_id: 1000,
+        permission_id: 1027
+    },
+    {
+        id: 1047,
+        role_id: 1000,
+        permission_id: 1028
+    },
+    {
+        id: 1048,
+        role_id: 1000,
+        permission_id: 1029
+    },
+    {
+        id: 1049,
+        role_id: 1001,
+        permission_id: 1029
+    },
+    {
+        id: 1050,
+        role_id: 1000,
+        permission_id: 1030
+    },
+    {
+        id: 1051,
+        role_id: 1000,
+        permission_id: 1031
+    },
+    {
+        id: 1052,
+        role_id: 1001,
+        permission_id: 1031
+    },
+    {
+        id: 1053,
+        role_id: 1000,
+        permission_id: 1032
+    },
+    {
+        id: 1054,
+        role_id: 1000,
+        permission_id: 1033
+    },
+    {
+        id: 1055,
+        role_id: 1000,
+        permission_id: 1034
+    },
+    {
+        id: 1056,
+        role_id: 1001,
+        permission_id: 1034
+    },
+    {
+        id: 1057,
+        role_id: 1000,
+        permission_id: 1035
+    },
+    {
+        id: 1058,
+        role_id: 1000,
+        permission_id: 1036
+    },
+    {
+        id: 1059,
+        role_id: 1000,
+        permission_id: 1037
+    },
+    {
+        id: 1060,
+        role_id: 1000,
+        permission_id: 1038
+    },
+    {
+        id: 1061,
+        role_id: 1001,
+        permission_id: 1038
+    },
+    {
+        id: 1062,
+        role_id: 1000,
+        permission_id: 1039
+    },
+    {
+        id: 1063,
+        role_id: 1000,
+        permission_id: 1040
+    },
+    {
+        id: 1064,
+        role_id: 1001,
+        permission_id: 1040
+    },
+    {
+        id: 1065,
+        role_id: 1000,
+        permission_id: 1041
+    },
+    {
+        id: 1066,
+        role_id: 1000,
+        permission_id: 1042
+    },
+    {
+        id: 1067,
+        role_id: 1001,
+        permission_id: 1042
+    },
+    {
+        id: 1068,
+        role_id: 1000,
+        permission_id: 1043
+    },
+    {
+        id: 1069,
+        role_id: 1000,
+        permission_id: 1044
+    },
+    {
+        id: 1070,
+        role_id: 1001,
+        permission_id: 1044
+    },
+    {
+        id: 1071,
+        role_id: 1000,
+        permission_id: 1045
+    },
+    {
+        id: 1072,
+        role_id: 1000,
+        permission_id: 1046
+    },
+    {
+        id: 1073,
+        role_id: 1001,
+        permission_id: 1046
+    },
+    {
+        id: 1074,
+        role_id: 1000,
+        permission_id: 1047
+    },
+    {
+        id: 1075,
+        role_id: 1000,
+        permission_id: 1048
+    },
+    {
+        id: 1076,
+        role_id: 1001,
+        permission_id: 1048
+    },
+    {
+        id: 1077,
+        role_id: 1000,
+        permission_id: 1049
+    },
+    {
+        id: 1078,
+        role_id: 1001,
+        permission_id: 1049
+    },
+    {
+        id: 1079,
+        role_id: 1000,
+        permission_id: 1050
+    },
+    {
+        id: 1080,
+        role_id: 1001,
+        permission_id: 1050
+    },
+    {
+        id: 1081,
+        role_id: 1000,
+        permission_id: 1051
+    },
+    {
+        id: 1082,
+        role_id: 1000,
+        permission_id: 1052
+    },
+    {
+        id: 1083,
+        role_id: 1001,
+        permission_id: 1052
+    },
+    {
+        id: 1084,
+        role_id: 1000,
+        permission_id: 1053
+    },
+    {
+        id: 1085,
+        role_id: 1000,
+        permission_id: 1054
+    },
+    {
+        id: 1086,
+        role_id: 1001,
+        permission_id: 1054
+    },
+    {
+        id: 1087,
+        role_id: 1000,
+        permission_id: 1055
+    },
+    {
+        id: 1088,
+        role_id: 1000,
+        permission_id: 1056
+    },
+    {
+        id: 1089,
+        role_id: 1001,
+        permission_id: 1056
+    },
+    {
+        id: 1090,
+        role_id: 1000,
+        permission_id: 1057
+    },
+    {
+        id: 1091,
+        role_id: 1000,
+        permission_id: 1058
+    },
+    {
+        id: 1092,
+        role_id: 1000,
+        permission_id: 1059
+    },
+    {
+        id: 1093,
+        role_id: 1001,
+        permission_id: 1059
+    },
+    {
+        id: 1094,
+        role_id: 1000,
+        permission_id: 1060
+    },
+    {
+        id: 1095,
+        role_id: 1000,
+        permission_id: 1061
+    },
+    {
+        id: 1096,
+        role_id: 1000,
+        permission_id: 1062
+    },
+    {
+        id: 1097,
+        role_id: 1000,
+        permission_id: 1063
+    },
+    {
+        id: 1098,
+        role_id: 1000,
+        permission_id: 1064
+    },
+    {
+        id: 1099,
+        role_id: 1001,
+        permission_id: 1064
+    },
+    {
+        id: 1100,
+        role_id: 1000,
+        permission_id: 1065
+    },
+    {
+        id: 1101,
+        role_id: 1001,
+        permission_id: 1065
+    },
+    {
+        id: 1102,
+        role_id: 1000,
+        permission_id: 1066
+    },
+    {
+        id: 1103,
+        role_id: 1001,
+        permission_id: 1066
+    },
+    {
+        id: 1104,
+        role_id: 1000,
+        permission_id: 1067
+    },
+    {
+        id: 1105,
+        role_id: 1000,
+        permission_id: 1068
+    },
+    {
+        id: 1106,
+        role_id: 1001,
+        permission_id: 1068
+    }
+];
 
 export default rolePermissions;
