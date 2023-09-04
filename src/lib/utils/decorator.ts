215a4529b3dd29ec81d3d5cf6c1308d9f487fd61
tora-o <97511605+tora-o@users.noreply.github.com>
2023-09-04 11:08:52 +0800

    feat: created decorator for error checking

--- src/lib/utils/decorator.ts
+++ src/lib/utils/decorator.ts
@@ -1,6 +1,7 @@
 import { NextRequest } from "next/server";
 import Response from "@lib/http";
 import PermissionController from "@controller/permission.controller";
+import logger from "@utils/logging";
 
 /**
  * Wrapper function to check if the user is permitted to access the resource
@@ -18,7 +19,7 @@ export function withPermission<T extends Function>(target: T): T {
 }
 
 /**
- * Decorator to check if the user is permitted to access the resource
+ * Class decorator to check if the user is permitted to access the resource
  *
  * @param target
  * @constructor
@@ -26,7 +27,7 @@ export function withPermission<T extends Function>(target: T): T {
 export function AllowPermitted(target: Function): void;
 
 /**
- * Decorator to check if the user is permitted to access the resource (method)
+ * Method decorator to check if the user is permitted to access the resource
  *
  * @param target
  * @param propertyKey
@@ -85,7 +86,7 @@ export function checkMethod<T extends Function>(func: T, method: string | string
 }
 
 /**
- * Decorator to check if the request body is valid
+ * Method decorator to check if the request body is valid
  *
  * @param method
  * @constructor
@@ -121,7 +122,7 @@ export function checkBody<T extends Function>(func: T): T {
 }
 
 /**
- * Decorator to check if the request body is valid
+ * Method decorator to check if the request body is valid
  *
  * @param _target
  * @param _propertyKey
@@ -131,4 +132,71 @@ export function checkBody<T extends Function>(func: T): T {
 export function CheckBody(_target: any, _propertyKey: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     descriptor.value = checkBody(originalMethod);
+}
+
+
+/**
+ * Wrapper function to check for errors
+ *
+ * @param func
+ */
+export function checkError<T extends Function>(func: Function): T {
+    const wrapped = async function (this: typeof func, request: NextRequest) {
+        try {
+            return await func.call(this, request);
+        } catch (err: any) {
+            logger.error(err);
+            return Response.internalServerError(err.message);
+        }
+    }
+
+    return wrapped as unknown as T;
+}
+
+/**
+ * Class decorator to check for errors
+ *
+ * @param target
+ * @constructor
+ */
+export function CheckError(target: Function): void;
+/**
+ * Method decorator to check for errors
+ *
+ * @param target
+ * @param propertyKey
+ * @param descriptor
+ * @constructor
+ */
+export function CheckError(target: any, propertyKey: string, descriptor: PropertyDescriptor): void;
+export function CheckError(...args: any[]): any {
+    if (args.length === 1 && typeof args[0] === 'function') {
+        // Class decorator or wrapper function
+        const target = args[0];
+        // Get all property keys (method names) of the class prototype
+        const keys = Object.getOwnPropertyNames(target.prototype);
+
+        // Loop through each method of the class
+        for (const key of keys) {
+            // Store a reference to the original method
+            const originalMethod = target.prototype[key];
+
+            // Create a new method that wraps the original method with your middleware
+            const newMethod = checkError(originalMethod)
+
+            // Replace the original method with the new wrapped method
+            Object.defineProperty(target.prototype, key, {
+                value: newMethod,
+                configurable: true,
+                writable: true,
+            });
+        }
+    } else if (args.length === 3 && typeof args[2] === 'object' && typeof args[2].value === 'function') {
+        const [_target, _propertyKey, descriptor] = args;
+        const originalMethod = descriptor.value;
+
+        descriptor.value = checkError(originalMethod);
+    } else {
+        throw new Error('Invalid usage of AllowPermitted decorator');
+    }
 }
