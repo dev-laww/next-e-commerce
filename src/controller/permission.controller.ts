61beaf6eef5f91af3a31b7782dc200a8983b5e32
tora-o <97511605+tora-o@users.noreply.github.com>
2023-09-04 10:55:52 +0800

    fix: fixed to pass tests

--- src/controller/permission.controller.ts
+++ src/controller/permission.controller.ts
@@ -5,6 +5,7 @@ import PermissionRepository from "@repository/permission.repo";
 import { getLogger } from "@utils/logging";
 import { verifyAccessToken } from "@utils/token";
 import { COMMON_RESOURCES } from "@lib/constants";
+import { UserSession } from "@lib/types";
 
 /**
  * Controller for permission
@@ -45,13 +46,17 @@ export default class PermissionController {
 
         if (!requestedResource || !token) return false;
 
-        const session = await verifyAccessToken(token);
-
-        if (!session) return false;
+        let session: UserSession;
+        try {
+            session = await verifyAccessToken(token);
+        } catch (err) {
+            logger.info("Failed to verify access token");
+            return false;
+        }
 
         const permissions = await this.repo.getPermissions(session.id);
 
-        if (!permissions) return false;
+        if (!permissions || permissions.length === 0) return false;
 
         logger.debug(permissions.map(permission => permission.resource), `${session.username} allowed resources`);
         return permissions.some(permission => permission.resource == requestedResource);
