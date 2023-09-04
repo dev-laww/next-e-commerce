aefec293e71aa4f65542b488c5464a378ee588b2
tora-o <97511605+tora-o@users.noreply.github.com>
2023-09-04 18:00:34 +0800

    chore: created validators for search params

--- src/lib/validator/accounts.validator.ts
+++ src/lib/validator/accounts.validator.ts
@@ -1,11 +1,33 @@
 import z from "zod";
 
-const testSchema = z.object({
-  name: z.string().min(2).max(255),
-  email: z.string().email(),
-  age: z.number().min(18).max(99),
+
+const search = z.object({
+    id: z.preprocess(
+        (a) => parseInt(a as string, 10),
+        z.number().positive()
+    ).optional(),
+    first_name: z.string().optional(),
+    last_name: z.string().optional(),
+    image_url: z.string().optional(),
+    email: z.string().optional(),
+    username: z.string().optional(),
+    confirmed: z.preprocess(
+        (a) => (a as string).match(/true/i) !== null,
+        z.boolean()
+    ).optional(),
+    pageToken: z.string().optional(),
+    limit: z.preprocess(
+        (a) => parseInt(a as string, 10),
+        z.number().positive()
+    ).optional(),
+    previous: z.preprocess(
+        (a) => (a as string).match(/true/i) !== null,
+        z.boolean()
+    ).optional(),
 });
 
-export default {
-    testSchema
-}
+const validator = {
+    search
+};
+
+export default validator;
