6b277ad9a6a79c4dfa4cbb87b48eda940e97a42f
tora-o <97511605+tora-o@users.noreply.github.com>
2023-09-05 21:01:25 +0800

    fix: build fail fix

--- src/app/api/accounts/[id]/route.ts
+++ src/app/api/accounts/[id]/route.ts
@@ -2,48 +2,41 @@ import { type NextRequest, NextResponse } from "next/server";
 
 import AccountsController from "@controller/accounts.controller";
 import { STATUS_CODE } from "@lib/constants";
-import Response from "@lib/http";
 import { getLogger } from "@utils/logging";
-
 
 const logger = getLogger({ name: "api:accounts:id" });
 
 
-export async function handler(req: NextRequest, { params }: { params: { id: string } }) {
-    const method = req.method;
-    let statusCode: number;
-    let response: any;
-    let success: boolean;
-
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
     const controller = new AccountsController();
 
-    switch (method) {
-        case "GET":
-            ({ statusCode, response } = await controller.getAccount(req, params));
-            success = statusCode == STATUS_CODE.OK;
-            logger.info(success ? response.message : response, success ? undefined : `Get account failed: ${response.message}`);
-            break;
-        case "PUT":
-            ({ statusCode, response } = await controller.updateAccount(req, params));
-            success = statusCode == STATUS_CODE.OK;
-            logger.info(success ? response.message : response, success ? undefined : `Update account failed: ${response.message}`);
-            break;
-        case "DELETE":
-            ({ statusCode, response } = await controller.deleteAccount(req, params));
-            success = statusCode == STATUS_CODE.OK;
-            logger.info(success ? response.message : response, success ? undefined : `Delete account failed: ${response.message}`);
-            break;
-        default:
-            ({ statusCode, response } = Response.methodNotAllowed);
-            logger.warn(`Method ${method} not supported`);
-    }
+    const { statusCode, response } = await controller.getAccount(req, params);
+    const success = statusCode == STATUS_CODE.OK;
+
+    logger.info(success ? response.message : response, success ? undefined : `Get account failed: ${response.message}`);
 
     return NextResponse.json(response, { status: statusCode })
 }
 
-export {
-    handler as GET,
-    handler as PUT,
-    handler as DELETE,
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+    const controller = new AccountsController();
+
+    const { statusCode, response } = await controller.updateAccount(req, params);
+    const success = statusCode == STATUS_CODE.OK;
+
+    logger.info(success ? response.message : response, success ? undefined : `Update account failed: ${response.message}`);
+
+    return NextResponse.json(response, { status: statusCode })
+}
+
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+    const controller = new AccountsController();
+
+    const { statusCode, response } = await controller.deleteAccount(req, params);
+    const success = statusCode == STATUS_CODE.OK;
+
+    logger.info(success ? response.message : response, success ? undefined : `Delete account failed: ${response.message}`);
+
+    return NextResponse.json(response, { status: statusCode })
 }
 
