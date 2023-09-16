651cfb3c8dabc8d96975e24d48362b007b2b879d
tora-o <97511605+tora-o@users.noreply.github.com>
2023-09-16 18:05:05 +0800

    feat: created more profile controller method

--- src/controller/profile.controller.ts
+++ src/controller/profile.controller.ts
@@ -2,12 +2,13 @@ import { type NextRequest } from "next/server";
 
 import Validators from "@lib/validator/profile.validator";
 import Response from "@lib/http";
-import { AllowPermitted, CheckError } from "@utils/decorator";
-import { generatePageToken, parsePageToken, verifyAccessToken } from "@utils/token";
+import { AllowPermitted, CheckBody, CheckError } from "@utils/decorator";
+import { verifyAccessToken } from "@utils/token";
 import Repository from "@src/repository";
 import { hash } from "@utils/hashing";
-import { PageToken } from "@lib/types";
-import { Address, User } from "@prisma/client";
+import { Address, Prisma } from "@prisma/client";
+import { ORDER_STATUS, PAYMENT_METHODS } from "@lib/constants";
+import humps from "humps";
 
 const getSession = async (req: NextRequest) => {
     const token = req.headers.get("Authorization")!.split(" ")[1];
@@ -26,10 +27,13 @@ export default class ProfileController {
         return Response.ok("Profile retrieved successfully", session);
     }
 
+    @CheckBody
     public async updateProfile(req: NextRequest) {
         const session = await getSession(req);
 
-        const profile = Validators.update.safeParse(req.body);
+        const body = await req.json();
+
+        const profile = Validators.update.safeParse(body);
 
         if (!profile.success) return Response.validationError(profile.error.errors);
 
@@ -52,15 +56,20 @@ export default class ProfileController {
 
         if (!passwordMatches) return Response.unauthorized("Password is incorrect");
 
-        const updatedProfile = await this.repo.update(session.id, profile.data);
+        delete body.password;
+
+        const updatedProfile = await this.repo.update(session.id, humps.decamelizeKeys(body));
 
         return Response.ok("Profile updated successfully", updatedProfile);
     }
 
+    @CheckBody
     public async updateEmail(req: NextRequest) {
         const session = await getSession(req);
 
-        const email = Validators.email.safeParse(req.body);
+        const body = await req.json();
+
+        const email = Validators.email.safeParse(body);
 
         if (!email.success) return Response.validationError(email.error.errors);
 
@@ -77,10 +86,13 @@ export default class ProfileController {
         return Response.ok("Email updated successfully", updatedProfile);
     }
 
-    public async updateUsename(req: NextRequest) {
+    @CheckBody
+    public async updateUsername(req: NextRequest) {
         const session = await getSession(req);
 
-        const username = Validators.username.safeParse(req.body);
+        const body = await req.json();
+
+        const username = Validators.username.safeParse(body);
 
         if (!username.success) return Response.validationError(username.error.errors);
 
@@ -88,16 +100,23 @@ export default class ProfileController {
 
         if (usernameExists) return Response.badRequest("Username already exists");
 
-        const updatedProfile = this.repo.update(session.id, username.data);
+        const passwordMatches = await this.repo.verifyPassword(session.id, username.data.password);
+
+        if (!passwordMatches) return Response.unauthorized("Password is incorrect");
+
+        const updatedProfile = this.repo.update(session.id, { username: username.data.username });
 
         return Response.ok("Username updated successfully", updatedProfile);
 
     }
 
+    @CheckBody
     public async updatePassword(req: NextRequest) {
         const session = await getSession(req);
 
-        const password = Validators.password.safeParse(req.body);
+        const body = await req.json();
+
+        const password = Validators.password.safeParse(body);
 
         if (!password.success) return Response.validationError(password.error.errors);
 
@@ -113,69 +132,303 @@ export default class ProfileController {
     }
 
     public async getAddresses(req: NextRequest) {
-        const searchParams = Object.fromEntries(req.nextUrl.searchParams);
+        const session = await getSession(req);
 
-        const filters = Validators.addresses.parse(searchParams);
+        const addresses = await this.repo.getAddresses(session.id);
 
-        let { pageToken, limit, ...filter } = filters;
-        limit = limit || 50;
+        return Response.ok("Addresses retrieved successfully", addresses);
+    }
 
-        // Parse page token
-        const parsedPageToken = parsePageToken<Address>(pageToken || "");
+    public async deleteAddresses(req: NextRequest) {
+        const session = await getSession(req);
 
-        let isPrevious;
-        if (pageToken) {
-            if (!parsedPageToken) return Response.badRequest("Invalid page token");
+        const addressesExists = await this.repo.getAddresses(session.id).then(addresses => addresses.length > 0);
 
-            isPrevious = parsedPageToken.type === "previous";
+        if (!addressesExists) return Response.notFound("No address found");
+
+        let addresses = await Repository.address.deleteUserAddresses(session.id);
+
+        return Response.ok(`${addresses.count} address${addresses.count > 1 ? "es" : ""} deleted successfully`);
+    }
+
+    @CheckBody
+    public async addAddress(req: NextRequest) {
+        const session = await getSession(req);
+
+        const body = await req.json();
+
+        const address = Validators.address.safeParse(body);
+
+        if (!address.success) return Response.validationError(address.error.errors);
+
+        const newAddress = await Repository.address.create(humps.decamelizeKeys({
+            ...address.data,
+            user_id: session.id
+        }) as Prisma.AddressCreateInput);
+
+        return Response.ok("Address added successfully", newAddress);
+    }
+
+    public async getAddress(req: NextRequest, params: { id: string }) {
+        const session = await getSession(req);
+
+        const { id } = params;
+
+        const address = await this.repo.getAddresses(session.id).then(addresses => addresses.find(address => address.id === Number(id) || 0));
+
+        if (!address) return Response.notFound("Address not found");
+
+        return Response.ok("Address retrieved successfully", address);
+    }
+
+    @CheckBody
+    public async updateAddress(req: NextRequest, params: { id: string }) {
+        const { id } = params;
+
+        let address = await Repository.address.getById(Number(id) || 0);
+
+        if (!address) return Response.notFound("Address not found");
+
+        const body = await req.json();
+
+        const updatedAddress = Validators.updateAddress.safeParse(body);
+
+        if (!updatedAddress.success) return Response.validationError(updatedAddress.error.errors);
+
+        address = await Repository.address.update(Number(id), humps.decamelizeKeys(updatedAddress.data));
+
+        return Response.ok("Address updated successfully", address);
+    }
+
+    public async deleteAddress(req: NextRequest, params: { id: string }) {
+        const { id } = params;
+
+        let address = await Repository.address.getById(Number(id) || 0);
+
+        if (!address) return Response.notFound("Address not found");
+
+        address = await Repository.address.delete(Number(id));
+
+        return Response.ok("Address deleted successfully", address);
+    }
+
+    public async getPaymentMethods(req: NextRequest) {
+        const session = await getSession(req);
+
+        const paymentMethods = await this.repo.getPaymentMethods(session.id);
+
+        return Response.ok("Payment methods retrieved successfully", paymentMethods);
+    }
+
+    public async deletePaymentMethods(req: NextRequest) {
+        const session = await getSession(req);
+
+        const paymentMethodsExists = await this.repo.getPaymentMethods(session.id).then(paymentMethods => paymentMethods.length > 0);
+
+        if (!paymentMethodsExists) return Response.notFound("No payment method found");
+
+        let paymentMethods = await Repository.paymentMethod.deleteByUserId(session.id);
+
+        return Response.ok(`${paymentMethods.count} payment method${paymentMethods.count > 1 ? "s" : ""} deleted successfully`);
+    }
+
+    @CheckBody
+    public async addPaymentMethod(req: NextRequest) {
+        const body = await req.json();
+
+        const paymentMethod = Validators.paymentMethod.safeParse(body);
+
+        if (!paymentMethod.success) return Response.validationError(paymentMethod.error.errors);
+
+        let validator;
+        switch (paymentMethod.data.type) {
+            case PAYMENT_METHODS.GOOGLE:
+            case PAYMENT_METHODS.PAYPAL:
+                validator = Validators.paypalAndGoogle;
+                break;
+            case PAYMENT_METHODS.CREDIT_CARD:
+                validator = Validators.creditCard;
+                break;
         }
 
-        const pageSize = isPrevious ? -limit : limit;
+        const paymentMethodData = validator.safeParse(paymentMethod.data);
 
-        // Fetch the addresses
-        const result = await Repository.user.getAll(filter, pageSize, parsedPageToken?.cursor as User);
+        if (!paymentMethodData.success) return Response.validationError(paymentMethodData.error.errors);
 
-        if (!result.length) return Response.notFound("No address found");
+        const newPaymentMethod = await Repository.paymentMethod.create(humps.decamelizeKeys(paymentMethodData.data) as Prisma.PaymentMethodCreateInput);
 
-        // Determine if there are more pages
-        const hasNextPage = await this.repo.getAll(filter, limit, result[result.length - 1]).then(res => res.length > 0);
-        const hasPreviousPage = await this.repo.getAll(filter, -limit, result[0]).then(res => res.length > 0);
+        return Response.ok("Payment method added successfully", newPaymentMethod);
+    }
 
-        // Generate URLs
-        const nextPageToken: PageToken<Address> = {
-            cursor: {
-                id: result[result.length - 1].id
-            },
-            type: "next"
-        };
+    public async getPaymentMethod(req: NextRequest, params: { id: string }) {
+        const session = await getSession(req);
 
-        const previousPageToken: PageToken<Address> = {
-            cursor: {
-                id: result[0].id
-            },
-            type: "previous"
-        };
+        const { id } = params;
 
-        const nextSearchParams = new URLSearchParams({
-            ...searchParams,
-            pageToken: generatePageToken(nextPageToken)
+        const paymentMethod = await this.repo.getPaymentMethods(session.id).then(paymentMethods => paymentMethods.find(paymentMethod => paymentMethod.id === Number(id) || 0));
+
+        if (!paymentMethod) return Response.notFound("Payment method not found");
+
+        return Response.ok("Payment method retrieved successfully", paymentMethod);
+    }
+
+    @CheckBody
+    public async updatePaymentMethod(req: NextRequest, params: { id: string }) {
+        const { id } = params;
+
+        let paymentMethod = await Repository.paymentMethod.getById(Number(id) || 0);
+
+        if (!paymentMethod) return Response.notFound("Payment method not found");
+
+        const body = await req.json();
+
+        const updatedPaymentMethod = Validators.paymentMethod.safeParse(body);
+
+        if (!updatedPaymentMethod.success) return Response.validationError(updatedPaymentMethod.error.errors);
+        let validator;
+        switch (updatedPaymentMethod.data.type) {
+            case PAYMENT_METHODS.GOOGLE:
+            case PAYMENT_METHODS.PAYPAL:
+                validator = Validators.updatePaypalAndGoogle;
+                break;
+            case PAYMENT_METHODS.CREDIT_CARD:
+                validator = Validators.updateCreditCard;
+                break;
+        }
+
+        const paymentMethodData = validator.safeParse(updatedPaymentMethod.data);
+
+        if (!paymentMethodData.success) return Response.validationError(paymentMethodData.error.errors);
+
+        paymentMethod = await Repository.paymentMethod.update(Number(id), humps.decamelizeKeys(paymentMethodData.data));
+
+        return Response.ok("Payment method updated successfully", paymentMethod);
+    }
+
+    public async deletePaymentMethod(req: NextRequest, params: { id: string }) {
+        const { id } = params;
+
+        const paymentMethod = await Repository.paymentMethod.getById(Number(id) || 0);
+
+        if (!paymentMethod) return Response.notFound("Payment method not found");
+
+        await Repository.paymentMethod.delete(Number(id));
+
+        return Response.ok("Payment method deleted successfully");
+    }
+
+    public async getOrders(req: NextRequest) {
+        const session = await getSession(req);
+
+        const orders = await this.repo.getOrders(session.id);
+
+        return Response.ok("Orders retrieved successfully", orders);
+    }
+
+    public async getOrder(req: NextRequest, params: { id: string }) {
+        const session = await getSession(req);
+
+        const { id } = params;
+
+        const order = await this.repo.getOrders(session.id).then(orders => orders.find(order => order.id === Number(id) || 0));
+
+        if (!order) return Response.notFound("Order not found");
+
+        return Response.ok("Order retrieved successfully", order);
+    }
+
+    public async cancelOrder(req: NextRequest, params: { id: string }) {
+        const { id } = params;
+
+        let order = await Repository.order.getById(Number(id) || 0);
+
+        if (!order) return Response.notFound("Order not found");
+
+        if (order.status !== "pending") return Response.badRequest("Order cannot be cancelled");
+
+        order = await Repository.order.update(Number(id), { status: ORDER_STATUS.CANCELLED });
+
+        return Response.ok("Order cancelled successfully", order);
+    }
+
+    public async getWishlist(req: NextRequest) {
+        const session = await getSession(req);
+
+        const wishlist = await this.repo.getWishlist(session.id);
+
+        return Response.ok("Wishlist retrieved successfully", wishlist);
+    }
+
+    public async deleteWishlist(req: NextRequest) {
+        const session = await getSession(req);
+
+        const wishlistExists = await this.repo.getWishlist(session.id).then(wishlist => wishlist.length > 0);
+
+        if (!wishlistExists) return Response.notFound("No wishlist found");
+
+        let wishlist = await Repository.wishlist.deleteUserWishlist(session.id);
+
+        return Response.ok(`${wishlist.count} wishlist${wishlist.count > 1 ? "s" : ""} deleted successfully`);
+    }
+
+    public async getWishlistItem(req: NextRequest, params: { id: string }) {
+        const session = await getSession(req);
+
+        const { id } = params;
+
+        const wishlistItem = await this.repo.getWishlist(session.id).then(wishlist => wishlist.find(wishlistItem => wishlistItem.id === Number(id) || 0));
+
+        if (!wishlistItem) return Response.notFound("Wishlist item not found");
+
+        return Response.ok("Wishlist item retrieved successfully", wishlistItem);
+    }
+
+    public async deleteWishlistItem(req: NextRequest, params: { id: string }) {
+        const { id } = params;
+
+        const wishlistItem = await Repository.wishlist.getById(Number(id) || 0);
+
+        if (!wishlistItem) return Response.notFound("Wishlist item not found");
+
+        await Repository.wishlist.delete(Number(id));
+
+        return Response.ok("Wishlist item deleted successfully");
+    }
+
+    @CheckBody
+    public async moveWishlistItemToCart(req: NextRequest, params: { id: string }) {
+        const { id } = params;
+        const body = await req.json();
+
+        const reqData = Validators.moveWishlistItemToCart.safeParse(body);
+
+        if (!reqData.success) return Response.validationError(reqData.error.errors);
+
+        const wishlistItem = await Repository.wishlist.getById(Number(id) || 0);
+
+        if (!wishlistItem) return Response.notFound("Wishlist item not found");
+
+        const {created_at, updated_at, id: itemId, ...data} = wishlistItem;
+
+        const productVariant = await Repository.productVariant.getById(reqData.data.variantId);
+
+        if (!productVariant) return Response.notFound("Product variant not found");
+
+        const cartItemExists = await Repository.cart.getAll({
+            user_id: wishlistItem.user_id,
+            variant_id: reqData.data.variantId
         });
 
-        const previousSearchParams = new URLSearchParams({
-            ...searchParams,
-            pageToken: generatePageToken(previousPageToken)
-        });
+        if (!!cartItemExists.length) return Response.badRequest("Product variant already exists in cart, please update the quantity instead");
 
-        const meta = {
-            hasNextPage,
-            hasPreviousPage,
-            previousPageUrl: hasPreviousPage ? `${req.nextUrl.origin}/${req.nextUrl.pathname}?${previousSearchParams.toString()}` : undefined,
-            nextPageUrl: hasNextPage ? `${req.nextUrl.origin}/${req.nextUrl.pathname}?${nextSearchParams.toString()}` : undefined,
-        };
+        const cartItem = await Repository.cart.create(humps.decamelizeKeys({
+            ...data,
+            ...reqData.data,
+            total_price: productVariant.price * reqData.data.quantity,
+        }) as Prisma.CartItemCreateInput);
 
-        return Response.ok("Addresses found", {
-            result,
-            meta,
-        });
+        await Repository.wishlist.delete(Number(id));
+
+        return Response.ok("Wishlist item moved to cart successfully", cartItem);
     }
 }
--- src/lib/validator/profile.validator.ts
+++ src/lib/validator/profile.validator.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { PAYMENT_METHODS } from "@lib/constants";
 
 namespace Validators {
     export const update = z.object({
@@ -51,18 +52,128 @@ namespace Validators {
             .max(50, "Password must not exceed 50 characters"),
     });
 
-    export const addresses = z.object({
-        id: z.preprocess(
-            a => parseInt(a as string, 10),
-            z.number().positive()
-        )
-            .optional(),
+    export const address = z.object({
+        name: z.string({ required_error: "Name is required" })
+            .min(3, "Name must be at least 3 characters long")
+            .max(50, "Name must not exceed 50 characters"),
+        address: z.string({ required_error: "Address is required" })
+            .min(3, "Address must be at least 3 characters long")
+            .max(50, "Address must not exceed 50 characters"),
+        city: z.string({ required_error: "City is required" })
+            .min(3, "City must be at least 3 characters long")
+            .max(50, "City must not exceed 50 characters"),
+        state: z.string({ required_error: "State is required" })
+            .min(3, "State must be at least 3 characters long")
+            .max(50, "State must not exceed 50 characters"),
+        country: z.string({ required_error: "Country is required" })
+            .min(3, "Country must be at least 3 characters long")
+            .max(50, "Country must not exceed 50 characters"),
+        zip: z.string({ required_error: "Zip is required" })
+            .min(4, "Zip must be at least 4 characters long")
+            .max(5, "Zip must not exceed 5 characters"),
+        phone: z.string({ required_error: "Phone is required" })
+            .min(11, "Phone must be at least 11 characters long")
+            .max(11, "Phone must not exceed 11 characters")
+    });
 
-        pageToken: z.string().optional(),
-        limit: z.preprocess(
-            (a) => parseInt(a as string, 10),
-            z.number().positive()
-        ).optional()
+    export const updateAddress = z.object({
+        name: z.string()
+            .min(3, "Name must be at least 3 characters long")
+            .max(50, "Name must not exceed 50 characters")
+            .optional(),
+        address: z.string()
+            .min(3, "Address must be at least 3 characters long")
+            .max(50, "Address must not exceed 50 characters")
+            .optional(),
+        city: z.string()
+            .min(3, "City must be at least 3 characters long")
+            .max(50, "City must not exceed 50 characters")
+            .optional(),
+        state: z.string()
+            .min(3, "State must be at least 3 characters long")
+            .max(50, "State must not exceed 50 characters")
+            .optional(),
+        country: z.string()
+            .min(3, "Country must be at least 3 characters long")
+            .max(50, "Country must not exceed 50 characters")
+            .optional(),
+        zip: z.string()
+            .min(4, "Zip must be at least 4 characters long")
+            .max(5, "Zip must not exceed 5 characters")
+            .optional(),
+        phone: z.string()
+            .min(11, "Phone must be at least 11 characters long")
+            .max(11, "Phone must not exceed 11 characters")
+            .optional()
+    });
+
+    export const paymentMethod = z.object({
+        type: z.enum([PAYMENT_METHODS.CREDIT_CARD, PAYMENT_METHODS.GOOGLE, PAYMENT_METHODS.PAYPAL]),
+    });
+
+    export const paypalAndGoogle = z.object({
+        name: z.string({ required_error: "Name is required" })
+            .min(3, "Name must be at least 3 characters long")
+            .max(50, "Name must not exceed 50 characters"),
+        email: z.string({ required_error: "Email is required" })
+            .email("Email must be a valid email address")
+    });
+
+    export const creditCard = z.object({
+        name: z.string({ required_error: "Name is required" })
+            .min(3, "Name must be at least 3 characters long")
+            .max(50, "Name must not exceed 50 characters"),
+        cardNumber: z.string({ required_error: "Card number is required" })
+            .min(16, "Card number must be at least 16 characters long")
+            .max(16, "Card number must not exceed 16 characters"),
+        expirationDate: z.string({ required_error: "Expiration date is required" })
+            .min(5, "Expiration date must be at least 5 characters long")
+            .max(5, "Expiration date must not exceed 5 characters"),
+        cvv: z.string({ required_error: "CVV is required" })
+            .min(3, "CVV must be at least 3 characters long")
+            .max(3, "CVV must not exceed 3 characters")
+    });
+
+    export const updatePaypalAndGoogle = z.object({
+        name: z.string({ required_error: "Name is required" })
+            .min(3, "Name must be at least 3 characters long")
+            .max(50, "Name must not exceed 50 characters")
+            .optional(),
+        email: z.string({ required_error: "Email is required" })
+            .email("Email must be a valid email address")
+            .optional()
+    });
+
+    export const updateCreditCard = z.object({
+        name: z.string({ required_error: "Name is required" })
+            .min(3, "Name must be at least 3 characters long")
+            .max(50, "Name must not exceed 50 characters")
+            .optional(),
+        cardNumber: z.string({ required_error: "Card number is required" })
+            .min(16, "Card number must be at least 16 characters long")
+            .max(16, "Card number must not exceed 16 characters")
+            .optional(),
+        expirationDate: z.string({ required_error: "Expiration date is required" })
+            .min(5, "Expiration date must be at least 5 characters long")
+            .max(5, "Expiration date must not exceed 5 characters")
+            .optional(),
+        cvv: z.string({ required_error: "CVV is required" })
+            .min(3, "CVV must be at least 3 characters long")
+            .max(3, "CVV must not exceed 3 characters")
+            .optional()
+    });
+
+    export const moveWishlistItemToCart = z.object({
+        variantId: z.preprocess(
+            a => parseInt(a as string, 10),
+            z.number({ required_error: "Variant ID is required" })
+                .positive()
+        ),
+        quantity: z.preprocess(
+            a => parseInt(a as string, 10),
+            z.number({ required_error: "Quantity is required" })
+                .positive()
+        )
     });
 }
 
