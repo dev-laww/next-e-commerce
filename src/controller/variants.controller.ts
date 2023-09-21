b4a4962e7fd6979655bcf91e98ea898babe77fd6
tora-o <97511605+tora-o@users.noreply.github.com>
2023-09-21 22:44:24 +0800

    feat: created methods

--- src/controller/variants.controller.ts
+++ src/controller/variants.controller.ts
@@ -1,12 +1,153 @@
 import { NextRequest } from "next/server";
 
 import Validators from "@lib/validator/variants.validator";
-import Response from "@lib/http";
-import { CheckError } from "@utils/decorator";
+import Response, { getSession } from "@lib/http";
+import { AllowPermitted, CheckBody, CheckError } from "@utils/decorator";
+import Repository from "@src/repository";
+import { getDatabaseLogger } from "@utils/logging";
+import { generatePageToken, parsePageToken } from "@utils/token";
+import { ProductVariant } from "@prisma/client";
+import { PageToken } from "@lib/types";
+import humps from "humps";
 
+@AllowPermitted
 @CheckError
 export default class VariantsController {
-    public async hello(req: NextRequest) {
-        return Response.ok("Hello World");
+    private repo = Repository.productVariant;
+    private logger = getDatabaseLogger({ name: "controller:variants", class: "VariantsController" });
+
+    public async getVariants(req: NextRequest) {
+        const searchParams = Object.fromEntries(req.nextUrl.searchParams);
+
+        const filters = Validators.search.parse(searchParams);
+
+        let { pageToken, limit, ...filter } = filters;
+        limit = limit || 50;
+
+        // Parse page token
+        const parsedPageToken = parsePageToken<ProductVariant>(pageToken || "");
+
+        let isPrevious;
+        if (pageToken) {
+            if (!parsedPageToken) return Response.badRequest("Invalid page token");
+
+            isPrevious = parsedPageToken.type === "previous";
+        }
+
+        const pageSize = isPrevious ? -limit : limit;
+
+        // Fetch the addresses
+        const result = await Repository.productVariant.getAll(filter, pageSize, parsedPageToken?.cursor as ProductVariant);
+
+        if (!result.length) return Response.notFound("No account found");
+
+        // Determine if there are more pages
+        const hasNextPage = await this.repo.getAll(filter, limit, result[result.length - 1]).then(res => res.length > 0);
+        const hasPreviousPage = await this.repo.getAll(filter, -limit, result[0]).then(res => res.length > 0);
+
+        // Generate URLs
+        const nextPageToken: PageToken<ProductVariant> = {
+            cursor: {
+                id: result[result.length - 1].id
+            },
+            type: "next"
+        };
+
+        const previousPageToken: PageToken<ProductVariant> = {
+            cursor: {
+                id: result[0].id
+            },
+            type: "previous"
+        };
+
+        const nextSearchParams = new URLSearchParams({
+            ...searchParams,
+            pageToken: generatePageToken(nextPageToken)
+        });
+
+        const previousSearchParams = new URLSearchParams({
+            ...searchParams,
+            pageToken: generatePageToken(previousPageToken)
+        });
+
+        const meta = {
+            hasNextPage,
+            hasPreviousPage,
+            previousPageUrl: hasPreviousPage ? `${req.nextUrl.origin}/${req.nextUrl.pathname}?${previousSearchParams.toString()}` : undefined,
+            nextPageUrl: hasNextPage ? `${req.nextUrl.origin}/${req.nextUrl.pathname}?${nextSearchParams.toString()}` : undefined,
+        };
+
+        await this.logger.info(`Retrieved ${result.length} variants`);
+        return Response.ok("Accounts found", {
+            result,
+            meta,
+        });
+    }
+
+    @CheckBody
+    public async createVariant(req: NextRequest) {
+        const session = await getSession(req);
+
+        const body = await req.json();
+
+        const data = Validators.create.safeParse(body);
+
+        if (!data.success) return Response.validationError(data.error.errors, "Invalid data");
+
+        const result = await this.repo.create(humps.decamelizeKeys({
+            ...data.data,
+            imageUrl: "https://via.placeholder.com/150",
+        }) as ProductVariant);
+
+        await this.logger.info(result, `User ${session.id} created variant ${result.id}`, true);
+        return Response.created("Variant created", result);
+    }
+
+    public async getVariant(req: NextRequest, params: { id: string }) {
+        const { id } = params;
+
+        const result = await this.repo.getById(Number(id) || 0);
+
+        if (!result) return Response.notFound("Variant not found");
+
+        await this.logger.info(`Retrieved variant ${result.id}`);
+        return Response.ok("Variant found", result);
+    }
+
+    @CheckBody
+    public async updateVariant(req: NextRequest, params: { id: string }) {
+        const { id } = params;
+
+        const session = await getSession(req);
+
+        const body = await req.json();
+
+        let result = await this.repo.getById(Number(id));
+
+        if (!result) return Response.notFound("Variant not found");
+
+        const data = Validators.update.safeParse(body);
+
+        if (!data.success) return Response.validationError(data.error.errors, "Invalid data");
+
+        result = await this.repo.update(result.id, humps.decamelizeKeys(data.data) as ProductVariant);
+
+        await this.logger.info(result, `User ${session.id} updated variant ${result.id}`, true);
+        return Response.ok("Variant updated", result);
+    }
+
+    public async deleteVariant(req: NextRequest, params: { id: string }) {
+        const session = await getSession(req);
+
+        const { id } = params;
+
+        let result = await this.repo.getById(Number(id) || 0);
+
+        if (!result) return Response.notFound("Variant not found");
+
+        result = await this.repo.delete(result.id);
+
+        await this.logger.info(result,`User ${session.id} deleted variant ${result.id}`, true);
+        return Response.ok("Variant deleted", result);
     }
 }
--- src/lib/validator/variants.validator.ts
+++ src/lib/validator/variants.validator.ts
@@ -1,10 +1,56 @@
 import z from "zod";
 
 namespace Validators {
-    export const testSchema = z.object({
-        name: z.string().min(2).max(255),
-        email: z.string().email(),
-        age: z.number().min(18).max(99),
+    export const search = z.object({
+        id: z.preprocess(
+            a => parseInt(a as string, 10),
+            z.number().positive("ID must be positive")
+        ).optional(),
+        productId: z.preprocess(
+            a => parseInt(a as string, 10),
+            z.number().positive("ID must be positive")
+        ).optional(),
+        pageToken: z.string().optional(),
+        limit: z.number().optional()
+    });
+
+    // TODO: make this form data to accept file uploads
+    export const create = z.object({
+        imageUrl: z.string({required_error: "Image is required"})
+            .url(),
+        productId: z.number({required_error: "Product ID is required"})
+            .positive(),
+        price: z.number({required_error: "Price is required"})
+            .positive(),
+        rawPrice: z.number({required_error: "Raw price is required"})
+            .positive(),
+        stock: z.number({required_error: "Quantity is required"})
+            .positive(),
+        name: z.string({required_error: "Name is required"})
+            .min(3, "Name must be at least 3 characters long")
+            .max(50, "Name must not exceed 255 characters"),
+    });
+
+    export const update = z.object({
+        imageUrl: z.string({required_error: "Image is required"})
+            .url()
+            .optional(),
+        productId: z.number({required_error: "Product ID is required"})
+            .positive()
+            .optional(),
+        price: z.number({required_error: "Price is required"})
+            .positive()
+            .optional(),
+        rawPrice: z.number({required_error: "Raw price is required"})
+            .positive()
+            .optional(),
+        stock: z.number({required_error: "Quantity is required"})
+            .positive()
+            .optional(),
+        name: z.string({required_error: "Name is required"})
+            .min(3, "Name must be at least 3 characters long")
+            .max(50, "Name must not exceed 255 characters")
+            .optional(),
     });
 }
 
