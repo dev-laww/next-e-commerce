6a960142fa71327674bee79b1b7a1790960c164e
tora-o <97511605+tora-o@users.noreply.github.com>
2023-09-25 23:17:58 +0800

    feat: revamped products controller

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -1,23 +1,22 @@
 import { NextRequest } from "next/server";
-import { Product } from "@prisma/client";
+import { Product, ProductVariant, Review } from "@prisma/client";
 import humps from 'humps';
 
 import Validators from "@lib/validator/products.validator";
-import Response from "@lib/http";
+import Response, { getSession } from "@lib/http";
 import Repository from "@src/repository";
 import { getDatabaseLogger } from "@src/lib/utils/logging";
 import { PageToken } from "@src/lib/types";
 import { generatePageToken, parsePageToken } from "@src/lib/utils/token";
-import { CheckError, CheckBody, AllowMethod, AllowPermitted } from "@utils/decorator";
+import { AllowPermitted, CheckBody, CheckError } from "@utils/decorator";
 
 @AllowPermitted
 @CheckError
 export default class ProductsController {
     private logger = getDatabaseLogger({ name: "controller:products", class: "ProductsController" })
-    repo = Repository.product;
-    categoryRepo = Repository.category;
+    private repo = Repository.product;
+    private categoryRepo = Repository.category;
 
-    @AllowMethod("GET")
     public async getProducts(req: NextRequest) {
         const searchParams = Object.fromEntries(req.nextUrl.searchParams);
 
@@ -31,7 +30,7 @@ export default class ProductsController {
         if (cFilter.categories) {
             cFilter.categories = cFilter.categories
                 .split(",")
-                .map((categoryId: string) => parseInt(categoryId, 10) || 0)
+                .map((categoryId: string) => Number(categoryId) || 0)
         }
 
         let type: "next" | "previous" | undefined;
@@ -40,7 +39,7 @@ export default class ProductsController {
         if (pageToken) {
             if (!token) return Response.badRequest("Invalid page token");
 
-            const { type: tokenType, ...cursorData } = token;
+            const { type: tokenType } = token;
 
             type = tokenType;
         }
@@ -96,71 +95,66 @@ export default class ProductsController {
         });
     }
 
-    @AllowMethod("GET")
     public async getProduct(_req: NextRequest, params: { id: string }) {
         const { id } = params;
 
-        const product = await this.repo.getById(parseInt(id, 10) || 0);
+        const product = await this.repo.getById(Number(id) || 0);
 
         if (!product) return Response.notFound("Product not found");
 
+        await this.logger.info(`Product found [${product.id}]`);
         return Response.ok("Product found", product);
     };
 
-    @AllowMethod("POST")
     @CheckBody
     async createProduct(req: NextRequest) {
+        const session = await getSession(req);
         const body = await req.json();
-
         const requestData = Validators.create.safeParse(body);
 
         if (!requestData.success) return Response.validationError(requestData.error.errors);
 
-        const product = this.repo.create(humps.decamelizeKeys(body) as Product);
+        const product = await this.repo.create(humps.decamelizeKeys(body) as Product);
 
+        await this.logger.info(product, `User [${session.id}] created product [${product.id}]`, true);
         return Response.created("Product created successfully", product)
     }
 
-    @AllowMethod("PUT")
     @CheckBody
     async updateProduct(req: NextRequest, params: { id: string }) {
+        const session = await getSession(req);
         const { id } = params;
-
-        const productInfo = await this.repo.getById(parseInt(id, 10) || 0);
+        const productInfo = await this.repo.getById(Number(id) || 0);
 
         if (!productInfo) return Response.notFound("Product not found");
 
         const body = await req.json();
-
         const product = Validators.update.safeParse(body);
 
         if (!product.success) return Response.badRequest(product.error.message);
 
         const updatedProduct = this.repo.update(productInfo.id, humps.decamelizeKeys(body) as Product)
 
-        await this.logger.info(updatedProduct, `Product [${id}] has been updated`, true);
+        await this.logger.info(updatedProduct, `User [${session.id}] updated product [${id}]`, true);
         return Response.ok("Product update successful", updatedProduct);
     }
 
-    @AllowMethod("DELETE")
-    public async deleteProduct(_req: NextRequest, params: { id: string }) {
+    public async deleteProduct(req: NextRequest, params: { id: string }) {
+        const session = await getSession(req);
         const { id } = params;
-
-        const product = await this.repo.getById(parseInt(id, 10) || 0);
+        const product = await this.repo.getById(Number(id) || 0);
 
         if (!product) return Response.notFound("Product not found");
 
         const deletedProduct = await this.repo.delete(product.id);
 
-        await this.logger.info(deletedProduct, `Product [${id}] deleted`, true);
+        await this.logger.info(deletedProduct, `User [${session.id}] deleted product [${id}]`, true);
         return Response.ok("Product delete successful", deletedProduct);
     }
 
-    @AllowMethod("GET")
     public async getVariants(_req: NextRequest, params: { id: string }) {
         const { id } = params;
-
-        const product = await this.repo.getById(parseInt(id, 10) || 0);
+        const product = await this.repo.getById(Number(id) || 0);
 
         if (!product) return Response.notFound("Product not found");
 
@@ -168,30 +162,85 @@ export default class ProductsController {
 
         if (!variants.length) return Response.notFound("Product variants not found");
 
+        await this.logger.info(`Product variants found for product [${product.id}]`);
         return Response.ok("Product variants found", variants);  // idk if we need to check the length, we should just return the result as is, empty or not
     }
 
-    @AllowMethod("DELETE")
-    public async deleteVariant(_req: NextRequest, params: { id: string, variantId: string }) {
-        const { id, variantId } = params;
-
-        const product = await this.repo.getById(parseInt(id, 10) || 0);
+    @CheckBody
+    public async addVariant(req: NextRequest, params: { id: string }) {
+        const session = await getSession(req);
+        const { id } = params;
+        const product = await this.repo.getById(Number(id) || 0);
 
         if (!product) return Response.notFound("Product not found");
 
-        const deletedVariant = await this.repo.deleteVariant(product.id, parseInt(variantId, 10) || 0);
+        const body = await req.json();
+
+        const variant = Validators.variant.safeParse(body);
+
+        if (!variant.success) return Response.badRequest(variant.error.message);
+
+        const productVariant = await this.repo.addVariant(product.id, humps.decamelizeKeys(variant.data) as ProductVariant);
+
+        await this.logger.info(productVariant, `User [${session.id}] added product variant [${id}] for product [${product.id}]`, true);
+        return Response.created("Product variant added", productVariant);
+    }
+
+    @CheckBody
+    public async updateVariant(req: NextRequest, params: { id: string, variantId: string }) {
+        const { id, variantId } = params;
+        const session = await getSession(req);
+        const product = await this.repo.getById(Number(id) || 0);
+
+        if (!product) return Response.notFound("Product not found");
+
+        const variantInfo = await this.repo.getVariants(product.id).then(res => res.find(variant => variant.id === Number(variantId) || 0));
+
+        if (!variantInfo) return Response.notFound("Product variant not found");
+
+        const body = await req.json();
+        const variant = Validators.variantUpdate.safeParse(body);
+
+        if (!variant.success) return Response.badRequest(variant.error.message);
+
+        const updatedVariant = await Repository.productVariant.update(variantInfo.id ,humps.decamelizeKeys(variant.data) as ProductVariant);
+
+        await this.logger.info(updatedVariant, `User [${session.id}] updated product variant [${id}] for product [${product.id}]`, true);
+        return Response.ok("Product variant update successful", updatedVariant);
+    }
+
+    public async deleteVariant(req: NextRequest, params: { id: string, variantId: string }) {
+        const session = await getSession(req);
+        const { id, variantId } = params;
+        const product = await this.repo.getById(Number(id) || 0);
+
+        if (!product) return Response.notFound("Product not found");
+
+        const deletedVariant = await this.repo.deleteVariant(product.id, Number(variantId) || 0);
 
         if (!deletedVariant) return Response.notFound("Product variant not found")
 
-        await this.logger.info(deletedVariant, `Product variant [${id}] deleted`, true)
-        return Response.ok("Product variant delete successfull", deletedVariant)
+        await this.logger.info(deletedVariant, `User [${session.id}]product variant [${id}] for product [${product.id}]`, true)
+        return Response.ok("Product variant delete successful", deletedVariant)
     }
 
-    @AllowMethod("GET")
+    public async deleteVariants(req: NextRequest, params: { id: string }) {
+        const session = await getSession(req);
+        const { id } = params;
+        const product = await this.repo.getById(Number(id) || 0);
+
+        if (!product) return Response.notFound("Product not found");
+
+        const result = await Repository.productVariant.deleteProductVariants(product.id);
+
+        await this.logger.info(undefined, `User [${session.id}] deleted product variants [${id}] for product [${product.id}]`, true)
+        return Response.ok(`${result.count} review${result.count > 1 ? "s" : ""} deleted successfully`)
+    }
+
     public async getCategories(_req: NextRequest, params: { id: string }) {
         const { id } = params;
 
-        const product = await this.repo.getById(parseInt(id, 10) || 0);
+        const product = await this.repo.getById(Number(id) || 0);
 
         if (!product) return Response.notFound("Product not found");
 
@@ -200,36 +249,128 @@ export default class ProductsController {
         return Response.ok("Product categories found", categories);
     }
 
-    @AllowMethod("POST")
-    public async addCategory(_req: NextRequest, params: { id: string, categoryId: string }) {
+    public async addCategory(req: NextRequest, params: { id: string, categoryId: string }) {
+        const session = await getSession(req);
         const { id, categoryId } = params;
-
-        const product = await this.repo.getById(parseInt(id, 10) || 0);
+        const product = await this.repo.getById(Number(id) || 0);
 
         if (!product) return Response.notFound("Product not found")
 
-        const category = await this.categoryRepo.getById(parseInt(categoryId, 10) || 0);
+        const category = await this.categoryRepo.getById(Number(categoryId) || 0);
 
         if (!category) return Response.notFound("Category not found")
 
         const productCategory = await this.repo.addCategory(product.id, category.id);
 
-        return Response.ok("Category added to product", productCategory);
+        await this.logger.info(productCategory, `User [${session.id}] added category [${category.id}] to product [${product.id}]`, true);
+        return Response.ok("Category added to product");
     }
 
-    @AllowMethod("DELETE")
-    public async deleteCategory(_req: NextRequest, params: { id: string, categoryId: string }) {
+    public async deleteCategory(req: NextRequest, params: { id: string, categoryId: string }) {
+        const session = await getSession(req);
         const { id, categoryId } = params;
-
-        const product = await this.repo.getById(parseInt(id, 10) || 0);
+        const product = await this.repo.getById(Number(id) || 0);
 
         if (!product) return Response.notFound("Product not found");
 
-        const deletedCategory = await this.repo.deleteCategory(product.id, parseInt(categoryId, 10) || 0);
+        const deletedCategory = await this.repo.deleteCategory(product.id, Number(categoryId) || 0);
 
         if (!deletedCategory) return Response.notFound("Product category not found")
 
-        await this.logger.info(deletedCategory, `Product category [${id}] deleted`, true)
-        return Response.ok("Product category delete successfull", deletedCategory)
+        await this.logger.info(deletedCategory, `User [${session.id}] removed category [${categoryId}] from product [${product.id}]`, true);
+        return Response.ok("Product category removed")
+    }
+
+    public async getReviews(_req: NextRequest, params: { id: string }) {
+        const { id } = params;
+        const product = await this.repo.getById(Number(id) || 0);
+
+        if (!product) return Response.notFound("Product not found");
+
+        const reviews = await this.repo.getReviews(product.id)
+
+        if (!reviews.length) return Response.notFound("Product reviews not found");
+
+        await this.logger.info(`Product reviews found for product [${product.id}]`);
+        return Response.ok("Product reviews found", reviews);
+    }
+
+    public async deleteReviews(req: NextRequest, params: { id: string }) {
+        const session = await getSession(req);
+        const { id } = params;
+        const product = await this.repo.getById(Number(id) || 0);
+
+        if (!product) return Response.notFound("Product not found");
+
+        const result = await Repository.review.deleteProductReviews(product.id);
+
+        await this.logger.info(`User [${session.id}] deleted reviews for product [${product.id}]`, undefined,true);
+        return Response.ok(`${result.count} review${result.count > 1 ? "s" : ""} deleted successfully`);
+    }
+
+    @CheckBody
+    public async addReview(req: NextRequest, params: { id: string }) {
+        const session = await getSession(req);
+        const { id } = params;
+        const product = await this.repo.getById(Number(id) || 0);
+
+        if (!product) return Response.notFound("Product not found");
+
+        const body = await req.json();
+
+        const review = Validators.review.safeParse(body);
+
+        if (!review.success) return Response.badRequest(review.error.message);
+
+        const productReview = await Repository.review.create(humps.decamelizeKeys({
+            ...review.data,
+            productId: product.id,
+            userId: session.id,
+        }) as Review);
+
+        await this.logger.info(productReview, `User [${session.id}] added review [${productReview.id}] for product [${product.id}]`, true);
+        return Response.created("Product review added", productReview);
+    }
+
+    @CheckBody
+    public async updateReview(req: NextRequest, params: { id: string, reviewId: string }) {
+        const { id, reviewId } = params;
+        const session = await getSession(req);
+        const product = await this.repo.getById(Number(id) || 0);
+
+        if (!product) return Response.notFound("Product not found");
+
+        const reviewInfo = await Repository.review.getById(Number(reviewId) || 0);
+
+        if (!reviewInfo) return Response.notFound("Product review not found");
+
+        const body = await req.json();
+        const review = Validators.reviewUpdate.safeParse(body);
+
+        if (!review.success) return Response.badRequest(review.error.message);
+
+        const updatedReview = await Repository.review.update(reviewInfo.id, humps.decamelizeKeys(review.data) as Review);
+
+        await this.logger.info(updatedReview, `User [${session.id}] updated review [${reviewId}] for product [${product.id}]`, true);
+        return Response.ok("Product review update successful", updatedReview);
+    }
+
+    public async deleteReview(req: NextRequest, params: { id: string, reviewId: string }) {
+        const session = await getSession(req);
+        const { id, reviewId } = params;
+        const product = await this.repo.getById(Number(id) || 0);
+
+        if (!product) return Response.notFound("Product not found");
+
+        const review = await Repository.review.getById(Number(reviewId) || 0);
+
+        if (!review) return Response.notFound("Product review not found");
+
+        const deletedReview = await Repository.review.delete(review.id);
+
+        if (!deletedReview) return Response.notFound("Product review not found")
+
+        await this.logger.info(deletedReview, `User [${session.id}] removed review [${reviewId}] from product [${product.id}]`, true);
+        return Response.ok("Product review removed")
     }
 }
