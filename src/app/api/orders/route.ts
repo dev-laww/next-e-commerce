e194d2a2ce1c1c18da2200f3e1bd5662474dd1af
tora-o <97511605+tora-o@users.noreply.github.com>
2023-09-30 21:40:47 +0800

    feat: finished routes for orders

---
+++ src/app/api/orders/route.ts
@@ -0,0 +1,20 @@
+import OrdersController from "@controller/orders.controller";
+import { NextRequest, NextResponse } from "next/server";
+import { getLogger } from "@utils/logging";
+import { STATUS_CODE } from "@lib/constants";
+
+const logger = getLogger({ name: "api:orders" });
+
+async function handler(req: NextRequest) {
+    const controller = new OrdersController();
+    const { statusCode, response } = await controller.getOrders(req);
+    const success = statusCode === STATUS_CODE.CREATED;
+
+    logger.info(success ? response.message : response, success ? undefined : `Get orders failed: ${response.message}`);
+    return NextResponse.json(response, { status: statusCode });
+}
+
+
+export {
+    handler as GET
+}
