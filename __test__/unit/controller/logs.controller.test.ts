989370cbcca47ef7fa964c429f96058e369dfbb5
tora-o <97511605+tora-o@users.noreply.github.com>
2023-09-30 22:17:35 +0800

    test: created tests for logs controller

--- __test__/unit/controller/logs.controller.test.ts
+++ __test__/unit/controller/logs.controller.test.ts
@@ -1,20 +1,67 @@
 import { NextRequest } from "next/server";
 import LogsController from "@controller/logs.controller";
+import { generateAccessToken } from "@utils/token";
+import { UserSession } from "@lib/types";
+import PermissionController from "@controller/permission.controller";
+import Response from "@lib/http";
+import Repository from "@src/repository";
+import { STATUS_CODE } from "@lib/constants";
 
+const isAllowed = jest.spyOn(PermissionController, "isAllowed");
 
 describe("LogsController", () => {
     let controller: LogsController;
     let req: NextRequest;
+    const token = generateAccessToken({ id: 1, username: "test" } as UserSession);
 
     beforeEach(() => {
         controller = new LogsController();
     });
 
     describe("Test getLogs", () => {
-        it.todo("returns 200 with logs");
-        it.todo("returns 401 if unauthorized");
-        it.todo("returns 403 if forbidden");
-        it.todo("returns 404 if no logs found");
+        beforeEach(() => {
+            req = new NextRequest("http://localhost:3000/api/logs", {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+        });
+
+        it("returns 200 with logs", async () => {
+            isAllowed.mockResolvedValueOnce(Response.ok());
+            (Repository.log.getAll as jest.Mock).mockImplementation(() => Promise.resolve([{ id: 1 }]));
+
+            const res = await controller.getLogs(req);
+
+            expect(res.statusCode).toBe(STATUS_CODE.OK);
+            expect(res.response).toBeDefined();
+        });
+
+        it("returns 401 if unauthorized", async () => {
+            isAllowed.mockResolvedValueOnce(Response.unauthorized());
+
+            const res = await controller.getLogs(req);
+
+            expect(res.statusCode).toBe(STATUS_CODE.UNAUTHORIZED);
+            expect(res.response).toBeDefined();
+        });
+
+        it("returns 403 if forbidden", async () => {
+            isAllowed.mockResolvedValueOnce(Response.forbidden);
+
+            const res = await controller.getLogs(req);
+
+            expect(res.statusCode).toBe(STATUS_CODE.FORBIDDEN);
+            expect(res.response).toBeDefined();
+        });
+
+        it("returns 404 if no logs found", async () => {
+            isAllowed.mockResolvedValueOnce(Response.ok());
+            (Repository.log.getAll as jest.Mock).mockImplementation(() => Promise.resolve([]));
+
+            const res = await controller.getLogs(req);
+
+            expect(res.statusCode).toBe(STATUS_CODE.NOT_FOUND);
+            expect(res.response).toBeDefined();
+        });
     });
 });
 
