7541592ce1cf8784e2625c5218c3fb950f434d19
tora-o <97511605+tora-o@users.noreply.github.com>
2023-09-30 22:11:43 +0800

    feat: finished logs controller

--- src/lib/validator/logs.validator.ts
+++ src/lib/validator/logs.validator.ts
@@ -1,10 +1,17 @@
 import z from "zod";
 
 namespace Validators {
-    export const testSchema = z.object({
-        name: z.string().min(2).max(255),
-        email: z.string().email(),
-        age: z.number().min(18).max(99),
+    export const search = z.object({
+        id: z.preprocess(
+            a => parseInt(a as string, 10),
+            z.number().positive("ID must be positive")
+        ).optional(),
+        level: z.string().optional(),
+        pageToken: z.string().optional(),
+        limit: z.preprocess(
+            a => parseInt(a as string, 10),
+            z.number().positive("Limit must be positive")
+        ).optional()
     });
 }
 
