615e5e7faacbf07297653c5f34a7041db5472faa
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-07 18:26:12 +0800

    feat: finished routes for coupons

---
+++ src/app/api/coupons/[code]/route.ts
@@ -0,0 +1,41 @@
+import CouponsController from "@controller/coupons.controller";
+import { NextRequest, NextResponse } from "next/server";
+import Response from "@lib/http";
+import { getLogger } from "@utils/logging";
+import { STATUS_CODE } from "@lib/constants";
+
+const logger = getLogger({ name: "api:coupons:code" });
+
+async function handler(req: NextRequest, { params }: { params: { code: string } }) {
+    const controller = new CouponsController();
+    let statusCode, response, success;
+
+    switch (req.method) {
+        case "GET":
+            ({ statusCode, response } = await controller.getCoupon(req, params));
+            success = statusCode === STATUS_CODE.OK;
+            logger.info(success ? response.message : response, success ? undefined : `Get coupon failed: ${response.message}`);
+            break;
+        case "PUT":
+            ({ statusCode, response } = await controller.updateCoupon(req, params));
+            success = statusCode === STATUS_CODE.OK;
+            logger.info(success ? response.message : response, success ? undefined : `Update coupon failed: ${response.message}`);
+            break;
+        case "DELETE":
+            ({ statusCode, response } = await controller.deleteCoupon(req, params));
+            success = statusCode === STATUS_CODE.OK;
+            logger.info(success ? response.message : response, success ? undefined : `Delete coupon failed: ${response.message}`);
+            break;
+        default:
+            ({ statusCode, response } = Response.methodNotAllowed);
+    }
+
+    return NextResponse.json(response, { status: statusCode });
+}
+
+
+export {
+    handler as GET,
+    handler as PUT,
+    handler as DELETE
+}
---
+++ src/app/api/coupons/route.ts
@@ -0,0 +1,35 @@
+import CouponsController from "@controller/coupons.controller";
+import { NextRequest, NextResponse } from "next/server";
+import Response from "@lib/http";
+import { getLogger } from "@utils/logging";
+import { STATUS_CODE } from "@lib/constants";
+
+const logger = getLogger({ name: "api:coupons" });
+
+async function handler(req: NextRequest) {
+    const controller = new CouponsController();
+    let statusCode, response, success;
+
+    switch (req.method) {
+        case "GET":
+            ({ statusCode, response } = await controller.getCoupons(req));
+            success = statusCode === STATUS_CODE.OK;
+            logger.info(success ? response.message : response, success ? undefined : `Get coupons failed: ${response.message}`);
+            break;
+        case "POST":
+            ({ statusCode, response } = await controller.createCoupon(req));
+            success = statusCode === STATUS_CODE.CREATED;
+            logger.info(success ? response.message : response, success ? undefined : `Create coupon failed: ${response.message}`);
+            break;
+        default:
+            ({ statusCode, response } = Response.methodNotAllowed);
+    }
+
+    return NextResponse.json(response, { status: statusCode });
+}
+
+
+export {
+    handler as GET,
+    handler as POST
+}
